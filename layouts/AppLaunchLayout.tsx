79fe1ababd140abb959a2069014372b01b8458bc
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-08-09 14:41:02 +0200

    upgrade avatar component to version that tolerates empty address

--- layouts/AppLaunchLayout.tsx
+++ layouts/AppLaunchLayout.tsx
@@ -3,20 +3,68 @@ import React, { FC, useRef, useState } from "react";
 import { FaWallet } from "react-icons/fa";
 import { useStore } from "lib/stores/Store";
 import AccountButton from "components/account/AccountButton";
+import { useAvatarContext } from "@zeitgeistpm/avatara-react";
+import { Avatar } from "@zeitgeistpm/avatara-nft-sdk";
+import { ZeitgeistAvatar } from "@zeitgeistpm/avatara-react";
+import { Skeleton } from "@material-ui/lab";
 
 const DefaultLayout: FC<{ launchDate: Date }> = observer(
   ({ children, launchDate }) => {
     const store = useStore();
+    const avataraContext = useAvatarContext();
+
+    const [isClaiming, setIsClaiming] = useState(false);
+    const [claimError, setClaimError] = useState<null | string>(null);
 
     const {
       wallets: { activeAccount },
     } = store;
 
+    const onClickClaim = async () => {
+      if (!activeAccount) {
+        return console.warn("no account");
+      }
+
+      setIsClaiming(true);
+      setClaimError(null);
+
+      try {
+        const response = await Avatar.claim(
+          avataraContext,
+          activeAccount.address,
+        );
+
+        if (!response?.avatar) {
+          setClaimError((response as any).message);
+        }
+      } catch (error) {
+        setClaimError(error.message);
+      }
+
+      setIsClaiming(false);
+    };
+
     return (
       <div className="relative flex min-h-screen justify-evenly bg-white overflow-hidden">
+        <div>
+          <div className="rounded-md overflow-hidden">
+            <ZeitgeistAvatar
+              address={activeAccount?.address || undefined}
+              size={200}
+              fallback={
+                <Skeleton
+                  style={{ transform: "none" }}
+                  height={"100%"}
+                  width={"100%"}
+                />
+              }
+            />
+          </div>
+        </div>
+
         <div className="absolute bottom-10 ">
           <AccountButton
-            connectButtonClassname="animate-pulse text-white flex w-ztg-168 h-ztg-40 bg-blue-500  text-black rounded-full text-ztg-14-150 font-medium items-center justify-center cursor-pointer disabled:cursor-default disabled:opacity-20"
+            connectButtonClassname="animate-pulse text-white flex w-ztg-184 h-12 bg-ztg-blue  text-black rounded-full text-ztg-18-150 font-medium items-center justify-center cursor-pointer disabled:cursor-default disabled:opacity-20"
             connectButtonText={
               <div className="flex items-center">
                 <FaWallet />
