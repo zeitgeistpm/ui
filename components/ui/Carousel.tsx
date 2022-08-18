77d32195d37aea48c3d99f723a399e0380aa3d1a
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2022-08-18 16:34:02 +0200

    fix build and remove old code

--- components/ui/Carousel.tsx
+++ components/ui/Carousel.tsx
@@ -26,43 +26,18 @@ const Carousel: FC<CarouselProps> = () => {
       <Slider className="rounded-ztg-10">
         <Slide index={0} className="bg-black rounded-ztg-10">
           <GlitchImage
-            src="/carousel/beta-welcome-2.png"
-            alt="Beta welcome"
+            glitchImageSrc="/carousel/beta-welcome-2.png"
             className="h-full rounded-ztg-10"
           />
         </Slide>
         <Slide index={1} className="bg-black rounded-ztg-10">
           <a href={`https://shapethefuture.zeitgeist.pm`} target="_blank">
             <GlitchImage
-              src="/carousel/shape-the-future.png"
-              alt="Shape the future with Zeitgeist"
+              glitchImageSrc="/carousel/shape-the-future.png"
               className="h-full rounded-ztg-10"
             />
           </a>
         </Slide>
-        {/* <Slide index={1} className="bg-red-500 rounded-ztg-10">
-          <div className="flex flex-col">
-            <div>Test</div>
-            <div>Displaying</div>
-            <div>Components</div>
-            <Switch
-              leftLabel="Buy"
-              rightLabel="Sell"
-              initialSelection="right"
-              onLeftSideClick={() => {}}
-              onRightSideClick={() => {}}
-            />
-          </div>
-        </Slide>
-        <Slide index={2} className="bg-black rounded-ztg-10">
-          <div className="rounded-ztg-10">
-            <img
-              src="https://via.placeholder.com/240x127"
-              alt="logo"
-              className="w-full rounded-ztg-10"
-            />
-          </div>
-        </Slide> */}
       </Slider>
       <div className="w-2/3 relative bottom-3 ml-16%">
         <div className="flex flex-row item-center justify-center">
