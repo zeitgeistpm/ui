681b5fbef9623abac9987cb8027c996ba3701efd
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2022-08-30 17:49:16 +0200

    correct units and clean up old code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
 import { observer } from "mobx-react";
 import { NextPage } from "next";
-import React, { FC, useEffect, useState } from "react";
-import { from } from "rxjs";
+import React, { FC } from "react";
+
 import { Skeleton } from "@material-ui/lab";
 
 import { useStore } from "lib/stores/Store";
 import MarketsList from "components/markets/MarketsList";
-import AspectRatioImage from "components/ui/AspectRatioImage";
 import { useMarketsUrlQuery } from "lib/hooks/useMarketsUrlQuery";
 import TrendingMarkets from "components/markets/TrendingMarkets";
 import Image from "next/image";
@@ -16,6 +14,27 @@ import { TrendingMarketInfo } from "components/markets/TrendingMarketCard";
 import { GraphQLClient } from "graphql-request";
 import getTrendingMarkets from "lib/gql/trending-markets";
 import { getPopularCategories, TagCounts } from "lib/gql/popular-categories";
+
+export async function getStaticProps() {
+  const client = new GraphQLClient(
+    "https://processor.bsr.zeitgeist.pm/graphql",
+  );
+  const trendingMarkets = await getTrendingMarkets(client);
+
+  if (!trendingMarkets || trendingMarkets.length === 0) {
+    //prevent rerender if server isn't returning markets
+    throw new Error("Unable to fetch trending markets");
+  }
+
+  const categories = await getPopularCategories(client);
+  return {
+    props: {
+      trendingMarkets: trendingMarkets,
+      tagCounts: categories,
+    },
+    revalidate: 10 * 60, //10min
+  };
+}
 
 const Category = ({
   title,
@@ -115,86 +134,6 @@ const PopularCategories: FC<{ tagCounts: TagCounts }> = observer(
     );
   },
 );
-
-const FeaturedMarketContent: FC<{
-  marketId: number;
-  imageUrl: string;
-  title: string;
-  text: string;
-}> = observer(({ marketId, imageUrl, title, text }) => {
-  return (
-    <div className="ztg-transition hover:bg-grey-100 hover:scale-105 ">
-      <Link href={`/markets/${marketId}`}>
-        <a>
-          <AspectRatioImage
-            ratio={400 / 225}
-            className="w-full h-ztg-136 rounded-ztg-10 mb-ztg-22"
-            imageUrl={imageUrl}
-          />
-          <h6 className="text-ztg-20-150 font-bold font-space">{title}</h6>
-          <div className="text-ztg-12-150 font-lato text-sky-600">{text}</div>
-        </a>
-      </Link>
-    </div>
-  );
-});
-
-const FeaturedMarkets: FC = observer(() => {
-  return (
-    <div className="flex flex-col mt-ztg-30">
-      <h5 className="font-space font-bold text-ztg-28-120 mb-ztg-30">
-        Featured Markets
-      </h5>
-      <div className="flex">
-        <div className="cursor-pointer flex flex-col flex-ztg-basis-240 flex-grow flex-shrink mr-ztg-10 min-w-0">
-          <FeaturedMarketContent
-            marketId={22}
-            imageUrl="/featured/Kanaria_NFT.png"
-            title="Kanaria NFT Price Floor"
-            text="Crypto"
-          />
-        </div>
-        <div className="cursor-pointer flex flex-col flex-ztg-basis-240 flex-grow flex-shrink ml-ztg-10 mr-ztg-10 min-w-0">
-          <FeaturedMarketContent
-            marketId={21}
-            imageUrl="/featured/Polkadot.png"
-            title="Parachain Auction Announcement Date"
-            text="Crypto"
-          />
-        </div>
-        <div className="cursor-pointer flex flex-col flex-ztg-basis-240 flex-grow flex-shrink ml-ztg-10 min-w-0">
-          <FeaturedMarketContent
-            marketId={23}
-            imageUrl="/featured/Kusama.png"
-            title="3rd round of Kusama Parachain Auction"
-            text="Crypto"
-          />
-        </div>
-      </div>
-    </div>
-  );
-});
-
-export async function getStaticProps() {
-  const client = new GraphQLClient(
-    "https://processor.bsr.zeitgeist.pm/graphql",
-  );
-  const trendingMarkets = await getTrendingMarkets(client);
-
-  if (!trendingMarkets || trendingMarkets.length === 0) {
-    //prevent rerender if server isn't returning markets
-    throw new Error("Unable to fetch trending markets");
-  }
-
-  const categories = await getPopularCategories(client);
-  return {
-    props: {
-      trendingMarkets: trendingMarkets,
-      tagCounts: categories,
-    },
-    revalidate: 10 * 60 * 10, //10min
-  };
-}
 
 const IndexPage: NextPage<{
   trendingMarkets: TrendingMarketInfo[];
