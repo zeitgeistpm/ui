503b6b7da3e622e531495f0e554f13b4a1d9af40
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-08-30 21:59:43 +0200

    added some polyfills that updated rmrk-tools needed

--- next.config.js
+++ next.config.js
@@ -2,4 +2,19 @@ module.exports = {
   experimental: {
     scrollRestoration: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        pako: false,
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        zlib: require.resolve("browserify-zlib"),
+      };
+    }
+
+    return config;
+  },
 };
