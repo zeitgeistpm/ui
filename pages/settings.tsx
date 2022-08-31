8626178d8818a0a76f271e5a25f86c6fee62a9ca
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-08-31 15:55:58 +0200

    fix setting issue where it would error using dev gql instance

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -293,7 +293,7 @@ const EndpointsSettings = observer(() => {
 
   const getCustomGqlEndpoint = () => {
     const opt = gqlEndpointOption();
-    return opt.parachain === SupportedParachain.CUSTOM ? opt.value : "";
+    return opt?.parachain === SupportedParachain.CUSTOM ? opt.value : "";
   };
 
   const [customRpcUrl, setCustomRpcUrl] = useState<string>(() =>
