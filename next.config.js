6e13b7265051c698b5623052479d6b45c843ea50
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-01 12:33:30 +0200

    use nextjs native browser fallbacks

--- next.config.js
+++ next.config.js
@@ -4,14 +4,9 @@ module.exports = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
       config.resolve.fallback = {
         pako: false,
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        zlib: require.resolve("browserify-zlib"),
+        ...config.resolve.fallback,
       };
     }
 
