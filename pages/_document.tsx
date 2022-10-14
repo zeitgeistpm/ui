32d77ecc9c53df35785a65feff764c6f144a6c73
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2022-10-14 13:16:08 +0200

    preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,23 +50,28 @@ export default function Document() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Kanit:wght@700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Condiment&family=Lato:wght@100;300;400;700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Condiment&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
         />
       </Head>
       <body className="overflow-x-hidden max-w-full">
