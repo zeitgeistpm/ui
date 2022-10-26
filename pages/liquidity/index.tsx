ab11f3a4f38f15f8cfa786428eb69a012a428a55
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-10-26 21:50:32 +0200

    work

--- pages/liquidity/index.tsx
+++ pages/liquidity/index.tsx
@@ -1,9 +1,31 @@
+import { TableCell } from "@material-ui/core";
 import { useQuery } from "@tanstack/react-query";
 import { ZTG } from "@zeitgeistpm/sdk-next";
+import Table, { TableColumn, TableData } from "components/ui/Table";
 import { useSdkv2 } from "lib/hooks/useSdkv2";
 import { sortBy } from "lodash";
 import { observer } from "mobx-react";
 import { NextPage } from "next";
+import { useMemo } from "react";
+
+const columns: TableColumn[] = [
+  {
+    header: "Market Id",
+    accessor: "marketId",
+    type: "component",
+    alignment: "text-left",
+  },
+  {
+    header: "Composition",
+    accessor: "composition",
+    type: "paragraph",
+  },
+  {
+    header: "Liquidity",
+    accessor: "poolBalance",
+    type: "currency",
+  },
+];
 
 const LiquidityPools: NextPage = observer(() => {
   const sdk = useSdkv2();
@@ -29,20 +51,43 @@ const LiquidityPools: NextPage = observer(() => {
     },
   );
 
+  const tableData = useMemo<TableData[]>(() => {
+    return (
+      pools.data?.map((pool) => {
+        const index = assetIndex.data?.[pool.poolId];
+        return {
+          marketId: <span>{index?.market.slug || "..."}</span>,
+          composition: (
+            <span>
+              {index?.assets
+                .map((asset) => `${asset.percentage}% ${asset.category.ticker}`)
+                .join(" - ") || "..."}
+            </span>
+          ),
+          poolBalance: index ? (
+            {
+              value: index?.liquidity.div(ZTG).toNumber(),
+              usdValue: 0,
+            }
+          ) : (
+            <span>...</span>
+          ),
+        };
+      }) ?? []
+    );
+  }, [pools.data, assetIndex.data]);
+
   return (
     <div>
-      {pools.data?.map((pool) => {
-        return (
-          <div>
-            <div>
-              {pool.poolId}:{" "}
-              {assetIndex.data?.[pool.poolId].liquidity
-                .dividedBy(ZTG)
-                .toString()}
-            </div>
-          </div>
-        );
-      })}
+      <Table
+        data={tableData}
+        columns={columns}
+        // onRowClick={handleRowClick}
+        // onLoadMore={handleLoadMoreFromChain}
+        // hideLoadMore={graphQlEnabled || poolsStore.allPoolsShown}
+        // loadingMore={loadingPage}
+        // loadingNumber={graphQlEnabled ? 5 : query.pagination.pageSize}
+      />
     </div>
   );
 });
