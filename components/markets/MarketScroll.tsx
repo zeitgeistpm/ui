32adf1558bf14d4c743198756a22c46f344a4edc
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2022-11-16 11:52:21 +0100

    hide cards that aren't in view

--- components/markets/MarketScroll.tsx
+++ components/markets/MarketScroll.tsx
@@ -2,8 +2,6 @@ import { motion } from "framer-motion";
 import { useRef, useState } from "react";
 import { useResizeDetector } from "react-resize-detector";
 
-interface MarketCardData {}
-
 interface MarketScrollProps {
   question: string;
 }
@@ -42,6 +40,27 @@ const MarketScroll = ({
     });
   };
 
+  //todo: should always return true for mobile
+  const isInView = (index: number) => {
+    const xPosition = scrollLeft * -1;
+    const itemWidth = gap + cardWidth;
+    const cardsBeforeView = Math.ceil(xPosition / itemWidth);
+
+    //card is outside the view port to the left
+    console.log(index, cardsBeforeView);
+    if (index < cardsBeforeView) {
+      return false;
+    }
+    const cardsDisplayed = Math.ceil(containerWidth / itemWidth);
+
+    if (index >= cardsBeforeView + cardsDisplayed) {
+      return false;
+    }
+
+    return true;
+  };
+
+  console.log(scrollLeft);
   return (
     <div ref={containerRef} className="flex flex-col">
       <div className="flex items-center mb-ztg-30">
@@ -58,18 +77,30 @@ const MarketScroll = ({
         </div>
       </div>
       {/* <div className="flex h-[175px] gap-x-[30px] overflow-auto"> */}
-      <motion.div
-        ref={scrollRef}
-        animate={{ x: scrollLeft }}
-        transition={{ duration: 0.5, type: "tween" }}
-        className="flex h-[175px] gap-x-[30px] no-scroll-bar overflow-x-auto sm:overflow-x-visible"
-      >
-        {markets.map((market) => (
-          <div className="bg-anti-flash-white rounded-ztg-10 min-w-[320px] w-full">
-            {market.question}
-          </div>
-        ))}
-      </motion.div>
+      <div className="relative">
+        {/* <div
+          className="bg-gradient-to-r from-indigo-500 h-[175px] w-[1000px] absolute -left-[800px]"
+          style={{ zIndex: 10 }}
+        ></div> */}
+        <motion.div
+          ref={scrollRef}
+          animate={{ x: scrollLeft }}
+          transition={{ duration: 0.5, type: "tween" }}
+          className="flex h-[175px] gap-x-[30px] no-scroll-bar overflow-x-auto sm:overflow-x-visible"
+        >
+          {markets.map((market, index) => (
+            <div
+              key={index}
+              className={`bg-anti-flash-white rounded-ztg-10 min-w-[320px] w-full ztg-transition ${
+                isInView(index) === false ? "bg-red-500" : ""
+              }`}
+            >
+              {market.question}
+            </div>
+          ))}
+        </motion.div>
+        {/* <div className="bg-gradient-to-l from-indigo-500  h-[175px] w-[200px] absolute -right-[50px] top-[0px]"></div> */}
+      </div>
     </div>
   );
 };
