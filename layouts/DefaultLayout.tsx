e2b66bb1fbb18c10eac277f97d195e0a4ab64a24
Rob (Work) <robhyrk@Robs-MacBook-Pro.local>
2022-12-08 15:20:09 -0500

    refactor: move font optimization to app

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -17,9 +17,6 @@ import { useSdkv2 } from "lib/hooks/useSdkv2";
 import { usePrevious } from "lib/hooks/usePrevious";
 import { shouldScrollTop } from "lib/util/should-scroll";
 
-// font optimization from @next/font
-import { inter, kanit, roboto_mono } from "lib/util/fonts";
-
 const NOTIFICATION_MESSAGE = process.env.NEXT_PUBLIC_NOTIFICATION_MESSAGE;
 
 const DefaultLayout: FC = observer(({ children }) => {
@@ -49,67 +46,55 @@ const DefaultLayout: FC = observer(({ children }) => {
   }, [router.pathname, prevPathname, shouldScrollTop]);
 
   return (
-    <>
-      {/* loads optimized fonts for global access */}
-      <style jsx global>
-        {`
-          :root {
-            --font-inter: ${inter.style.fontFamily};
-            --font-kanit: ${kanit.style.fontFamily};
-            --font-roboto-mono: ${roboto_mono.style.fontFamily};
-          }
-        `}
-      </style>
-      <div
-        onScrollCapture={onScrollCapture}
-        className="relative flex min-h-screen justify-evenly bg-white dark:bg-sky-1000 overflow-hidden"
-      >
-        <TradeSlipStoreContext.Provider value={store.tradeSlipStore}>
-          <LeftDrawer />
-          <div
-            ref={contentRef}
-            className="overflow-y-auto overflow-x-hidden flex-grow"
+    <div
+      onScrollCapture={onScrollCapture}
+      className="relative flex min-h-screen justify-evenly bg-white dark:bg-sky-1000 overflow-hidden"
+    >
+      <TradeSlipStoreContext.Provider value={store.tradeSlipStore}>
+        <LeftDrawer />
+        <div
+          ref={contentRef}
+          className="overflow-y-auto overflow-x-hidden flex-grow"
+        >
+          <TopBar />
+          {NOTIFICATION_MESSAGE && (
+            <div className="sticky top-ztg-76 z-ztg-2 flex w-full justify-center items-center bg-yellow-100 h-ztg-38">
+              <div className="text-ztg-12-150 font-semibold">
+                {NOTIFICATION_MESSAGE}
+              </div>
+            </div>
+          )}
+          <main
+            className="main-container flex flex-col dark:text-white"
+            ref={mainRef}
           >
-            <TopBar />
-            {NOTIFICATION_MESSAGE && (
-              <div className="sticky top-ztg-76 z-ztg-2 flex w-full justify-center items-center bg-yellow-100 h-ztg-38">
-                <div className="text-ztg-12-150 font-semibold">
-                  {NOTIFICATION_MESSAGE}
-                </div>
-              </div>
-            )}
-            <main
-              className="main-container flex flex-col dark:text-white"
-              ref={mainRef}
-            >
-              <div className="max-w-ztg-1100 mx-auto py-0 px-ztg-32 pt-ztg-14 w-full ">
-                <ContentDimensionsProvider
-                  scrollTop={scrollTop}
-                  scrollTo={scrollTo}
-                  height={height}
-                  width={width}
-                >
-                  {store.initialized ||
-                  router.pathname === "/" ||
-                  router.pathname.split("/")[1] === "markets" ||
-                  router.pathname.split("/")[1] === "liquidity" ? (
-                    children
-                  ) : (
-                    <Skeleton
-                      className="!transform-none !mt-ztg-30"
-                      style={{ height: "550px" }}
-                    />
-                  )}
-                </ContentDimensionsProvider>
-              </div>
-              <Footer />
-            </main>
-          </div>
-          <RightDrawer />
-          <NotificationCenter />
-        </TradeSlipStoreContext.Provider>
-      </div>
-    </>
+            <div className="max-w-ztg-1100 mx-auto py-0 px-ztg-32 pt-ztg-14 w-full ">
+              <ContentDimensionsProvider
+                scrollTop={scrollTop}
+                scrollTo={scrollTo}
+                height={height}
+                width={width}
+              >
+                {store.initialized ||
+                router.pathname === "/" ||
+                router.pathname.split("/")[1] === "markets" ||
+                router.pathname.split("/")[1] === "liquidity" ? (
+                  children
+                ) : (
+                  <Skeleton
+                    className="!transform-none !mt-ztg-30"
+                    style={{ height: "550px" }}
+                  />
+                )}
+              </ContentDimensionsProvider>
+            </div>
+            <Footer />
+          </main>
+        </div>
+        <RightDrawer />
+        <NotificationCenter />
+      </TradeSlipStoreContext.Provider>
+    </div>
   );
 });
 
