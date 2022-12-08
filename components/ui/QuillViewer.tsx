1e3212f120cc4824615c644a68bd477ffa46b46d
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2022-12-08 11:30:52 +0100

    make formatting fix retroactive by adding to viewer

--- components/ui/QuillViewer.tsx
+++ components/ui/QuillViewer.tsx
@@ -2,7 +2,16 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 
 const QuillViewer = ({ value }: { value: string }) => {
-  return <ReactQuill theme="bubble" value={value} readOnly={true} />;
+  const formats = ["bold", "underline", "italic", "link", "list"];
+
+  return (
+    <ReactQuill
+      theme="bubble"
+      value={value}
+      readOnly={true}
+      formats={formats}
+    />
+  );
 };
 
 export default QuillViewer;
