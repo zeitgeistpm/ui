9bad4e04dcc0f28ddd3aa870b21003d3ec341c10
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-12-13 16:38:56 +0100

    use toCompositeIndexerAssetId to convert asset ids to indexer asset id string ids.

--- lib/hooks/queries/useAssetsPriceHistory.ts
+++ lib/hooks/queries/useAssetsPriceHistory.ts
@@ -4,9 +4,11 @@ import {
   HistoricalAssetOrderByInput,
   HistoricalAssetsQuery,
 } from "@zeitgeistpm/indexer";
-import { isCodec } from "@polkadot/util";
-import { AssetId, fromPrimitive, isIndexedSdk } from "@zeitgeistpm/sdk-next";
-import { camelcaseObjectKeys } from "lib/util/camelcase-object.keys";
+import {
+  AssetId,
+  isIndexedSdk,
+  toCompositeIndexerAssetId,
+} from "@zeitgeistpm/sdk-next";
 import { useSdkv2 } from "../useSdkv2";
 
 export const rootKey = "assets-price-history";
@@ -29,9 +31,7 @@ export const useAssetsPriceHistory = (
       if (isIndexedSdk(sdk)) {
         const history = await sdk.context.indexer.historicalAssets({
           where: {
-            assetId_in: assets.map((asset) =>
-              JSON.stringify(camelcaseObjectKeys(asset)),
-            ),
+            assetId_in: assets.map(toCompositeIndexerAssetId),
             timestamp_gte: filter?.startTimeStamp ?? "0",
           },
           order: HistoricalAssetOrderByInput.BlockNumberAsc,
@@ -39,13 +39,10 @@ export const useAssetsPriceHistory = (
 
         return {
           get: (assetId) => {
-            const key = JSON.stringify(
-              camelcaseObjectKeys(
-                isCodec(assetId) ? fromPrimitive(assetId) : assetId,
-              ),
-            );
             return history?.historicalAssets.filter((historicalAsset) => {
-              return historicalAsset.assetId === key;
+              return (
+                historicalAsset.assetId === toCompositeIndexerAssetId(assetId)
+              );
             });
           },
         };
