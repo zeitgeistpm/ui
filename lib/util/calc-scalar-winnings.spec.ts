40c3ff1cdc52aec8653a56ea6be3a73c9dd098f8
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2022-12-16 15:06:40 +0000

    fix typo

--- lib/util/calc-scalar-winnings.spec.ts
+++ lib/util/calc-scalar-winnings.spec.ts
@@ -1,15 +1,15 @@
 import { calcScalarWinnings } from "./calc-scalar-winnings";
 
 describe("calcScalarWinnings", () => {
-  test("should calculate winnings correct for short tokens", () => {
+  test("should calculate winnings correctly for short tokens", () => {
     const winnings = calcScalarWinnings(0, 10, 5, 100, 0);
     expect(winnings).toEqual(50);
   });
-  test("should calculate winnings correct for long tokens", () => {
+  test("should calculate winnings correctly for long tokens", () => {
     const winnings = calcScalarWinnings(0, 40, 10, 0, 100);
     expect(winnings).toEqual(25);
   });
-  test("should calculate winnings correct for both long and short tokens", () => {
+  test("should calculate winnings correctly for both long and short tokens", () => {
     const winnings = calcScalarWinnings(0, 40, 10, 100, 100);
     expect(winnings).toEqual(100);
   });
