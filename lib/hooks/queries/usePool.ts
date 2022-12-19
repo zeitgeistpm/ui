b0bf4998b79241e79911d948305bd13d1e7ad163
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-12-19 13:52:00 +0100

    cache pools

--- lib/hooks/queries/usePool.ts
+++ lib/hooks/queries/usePool.ts
@@ -1,14 +1,14 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { isIndexedSdk, isRpcData, PoolGetQuery } from "@zeitgeistpm/sdk-next";
+import { useQuery } from "@tanstack/react-query";
+import { isIndexedSdk, PoolGetQuery } from "@zeitgeistpm/sdk-next";
 import { useSdkv2 } from "../useSdkv2";
 
-export const rootKey = "pool";
+export const poolsRootKey = "pools";
 
 export const usePool = (getPoolQuery?: PoolGetQuery) => {
   const [sdk, id] = useSdkv2();
 
   const query = useQuery(
-    [id, rootKey, getPoolQuery],
+    [id, poolsRootKey, getPoolQuery],
     async () => {
       if (isIndexedSdk(sdk)) {
         const pool = await sdk.model.swaps.getPool(getPoolQuery);
