07d5afdd6fd0156f35ee146b4831d7680ecae36a
Rob (Work) <robhyrk@Robs-MacBook-Pro.local>
2022-12-21 19:27:31 -0500

    feat(hero): add slider content and styles

--- components/ui/HeroSlider.tsx
+++ components/ui/HeroSlider.tsx
@@ -98,22 +98,93 @@ const slides = [
 const HeroSlider: FC<HeroSliderProps> = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  const goToPrevious = () => {
+    const isFirstSlide = currentSlide === 0;
+    const newSlide = isFirstSlide ? slides.length - 1 : currentSlide - 1;
+    setCurrentSlide(newSlide);
+  };
+
+  const goToNext = () => {
+    const isFirstSlide = currentSlide === slides.length - 1;
+    const newSlide = isFirstSlide ? 0 : currentSlide + 1;
+    setCurrentSlide(newSlide);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+  };
+
   return (
     <section className="w-full h-[527px] mx-auto">
       <div className="h-full relative">
         <div
-          className="bg-cover bg-center h-full w-full"
+          className="bg-cover bg-center h-full w-full p-10"
           style={{ backgroundImage: `url(${slides[currentSlide].bg})` }}
-        ></div>
-        <div className="flex absolute bottom-[15%] right-[10%]">
+        >
+          <div className="max-w-[540px] pb-8">
+            <h2
+              style={{
+                color: `${slides[currentSlide].title.color}`,
+                fontSize: `${slides[currentSlide].title.size}`,
+              }}
+              className="font-kanit mb-6 leading-tight"
+            >
+              {slides[currentSlide].title.text}
+            </h2>
+            <div className="flex flex-col md:flex-row">
+              <a
+                style={{
+                  borderColor: `${slides[currentSlide].color1.border}`,
+                  backgroundColor: `${slides[currentSlide].color1.primary}`,
+                  color: `${slides[currentSlide].color1.secondary}`,
+                }}
+                className="w-fit border rounded py-2 px-5 mb-5 mr-5 font-bold"
+                href={slides[currentSlide].link1}
+                target="_blank"
+              >
+                {slides[currentSlide].cta1}
+              </a>
+              {slides[currentSlide].cta2 && (
+                <a
+                  style={{
+                    borderColor: `${slides[currentSlide].color2.border}`,
+                    backgroundColor: `${slides[currentSlide].color2.primary}`,
+                    color: `${slides[currentSlide].color2.secondary}`,
+                  }}
+                  className="w-fit border rounded py-2 px-3 mb-5 mr-5 font-bold"
+                  href={slides[currentSlide].link2}
+                  target="_blank"
+                >
+                  {slides[currentSlide].cta2}
+                </a>
+              )}
+            </div>
+          </div>
+          ;
+        </div>
+        <div className="flex items-center justify-center md:justify-end w-full mx-auto gap-1 p-10 absolute bottom-0">
           <button
-            className={`bg-black flex items-center justify-center w-[40px] h-[40px] rounded-full ztg-transition`}
+            onClick={goToPrevious}
+            className={`bg-black border border-white flex items-center justify-center w-[40px] h-[40px] rounded-full`}
           >
             <ChevronLeft className="text-white relative right-[1px]" />
           </button>
-          <span className="text-white">. . . .</span>
+          <div className="text-white">
+            {slides.map((slide, index) => (
+              <span
+                onClick={() => goToSlide(index)}
+                className={`cursor-pointer text-[48px] px-1 ${
+                  index === currentSlide ? "text-white" : "opacity-50"
+                }`}
+                key={index}
+              >
+                &middot;
+              </span>
+            ))}
+          </div>
           <button
-            className={`bg-black flex items-center justify-center w-[40px] h-[40px] rounded-full ztg-transition`}
+            onClick={goToNext}
+            className={`bg-black border border-white flex items-center justify-center w-[40px] h-[40px] rounded-full ztg-transition`}
           >
             <ChevronRight className="text-white relative left-[1px]" />
           </button>
