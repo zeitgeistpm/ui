d9cad84059e2355b60fb4a94c495052c27ad6781
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-01-05 11:44:13 +0000

    Refactor popular categories to use a single data source. Use placeholder image generation

--- components/front-page/PopularCategories.tsx
+++ components/front-page/PopularCategories.tsx
@@ -1,18 +1,27 @@
-import { TagCounts } from "lib/gql/popular-categories";
 import { observer } from "mobx-react";
 import { FC, useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
+import { IGetPlaiceholderReturn } from "plaiceholder";
+
+export const CATEGORIES = [
+  { name: "Crypto", imagePath: "/category/crypto.png" },
+  { name: "Governance", imagePath: "/category/governance.png" },
+  { name: "Politics", imagePath: "/category/politics.png" },
+  { name: "Sports", imagePath: "/category/sports.png" },
+] as const;
 
 const Category = ({
   title,
   imgURL,
+  blurImage,
   onClick,
   count,
 }: {
   title: string;
   imgURL: string;
+  blurImage: IGetPlaiceholderReturn;
   onClick: () => void;
   count: number;
   className?: string;
@@ -30,7 +39,13 @@ const Category = ({
         onClick={onClick}
       >
         <motion.div animate={isHoving ? { scale: 1.05 } : { scale: 1.0 }}>
-          <Image src={imgURL} alt={title} width={100} height={100} />
+          <Image
+            src={imgURL}
+            alt={title}
+            width={100}
+            height={100}
+            blurDataURL={blurImage.base64}
+          />
         </motion.div>
       </div>
       <span className="flex mt-[10px] items-center">
@@ -45,51 +60,37 @@ const Category = ({
   );
 };
 
-const PopularCategories: FC<{ tagCounts: TagCounts }> = observer(
-  ({ tagCounts }) => {
-    const router = useRouter();
+const PopularCategories: FC<{
+  counts: number[];
+  imagePlaceholders: IGetPlaiceholderReturn[];
+}> = observer(({ counts, imagePlaceholders }) => {
+  const router = useRouter();
 
-    const navigateToTag = (tag: string) => {
-      router.push({ pathname: "/markets", query: { tag } });
-    };
+  const navigateToTag = (tag: string) => {
+    router.push({ pathname: "/markets", query: { tag } });
+  };
 
-    return (
-      <div className="flex flex-col mt-ztg-30">
-        <h3 className=" font-bold text-[28px] mb-ztg-30">Popular Categories</h3>
-        <div
-          className="grid w-full gap-[28px]"
-          style={{
-            gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
-          }}
-        >
+  return (
+    <div className="flex flex-col mt-ztg-30">
+      <h3 className=" font-bold text-[28px] mb-ztg-30">Popular Categories</h3>
+      <div
+        className="grid w-full gap-[28px]"
+        style={{
+          gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
+        }}
+      >
+        {CATEGORIES.map((category, index) => (
           <Category
-            title="Sports"
-            imgURL="/topics/sports.png"
-            count={tagCounts.sports}
-            onClick={() => navigateToTag("Sports")}
+            title={category.name}
+            imgURL={category.imagePath}
+            blurImage={imagePlaceholders[index]}
+            count={counts[index]}
+            onClick={() => navigateToTag(category.name)}
           />
-          <Category
-            title="Politics"
-            imgURL="/topics/politics.png"
-            count={tagCounts.politics}
-            onClick={() => navigateToTag("Politics")}
-          />
-          <Category
-            title="Governance"
-            imgURL="/topics/governance.png"
-            count={tagCounts.governance}
-            onClick={() => navigateToTag("Governance")}
-          />
-          <Category
-            title="Crypto"
-            imgURL="/topics/crypto.png"
-            count={tagCounts.crypto}
-            onClick={() => navigateToTag("Crypto")}
-          />
-        </div>
+        ))}
       </div>
-    );
-  },
-);
+    </div>
+  );
+});
 
 export default PopularCategories;
