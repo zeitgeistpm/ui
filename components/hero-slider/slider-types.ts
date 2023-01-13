e5aa5f84fec40feb849b12b0ef1df5f14026e4fc
Rob Hyrkiel <rob@zeitgeist.pm>
2023-01-12 20:45:30 -0500

    530 update hero banner (#560)
    * feat(hero): create slides data, standardize slider, update styles
    
    * feat(hero): add padding to offset center content slightly
    
    * feat(hero): test mobile styles
    
    * feat(hero): add html structure to custom slider
    
    * feat(hero): add slider content and styles
    
    * feat)hero): add animation effect to slider on user interaction
    
    * feat(hero): add autoplay feature
    
    * refactor(hero): create seperate component, split into smaller files
    
    * feat(hero): enable ability to add custom slider within array of slides data
    
    * refactor(hero): add original carousel code back
    
    * refactor: import new hero banner in index, leave existing hero as default for now
    
    * feat: update slider types
    
    * refactor: remove slider stiles from global index.css into mocomponent level
    
    * docs: add usage details to hero slider and into comments
    
    * refactor: removed custom slide feat, adjusted HeroSlide to accept just current slide, renamed/removed files, adjusted TS interfaces, modified styles
    
    * chore: comment out new slider
    
    Co-authored-by: Rob (Work) <robhyrk@Robs-MacBook-Pro.local>

---
+++ components/hero-slider/slider-types.ts
@@ -0,0 +1,35 @@
+interface Title {
+  text: string;
+  size: string;
+  color: string;
+}
+interface Color {
+  primary: string;
+  secondary: string;
+  border: string;
+}
+
+type Animate = (val: boolean) => void;
+export interface Slides {
+  id: number;
+  title: Title;
+  bg: string;
+  cta1: string;
+  link1: string;
+  color1: Color;
+  cta2: string;
+  link2: string;
+  color2: Color;
+}
+export interface HeroSlideProps {
+  slide: Slides;
+  animate: boolean;
+  setAnimate: Animate;
+}
+export interface HeroControlsProps {
+  slides: Slides[];
+  slidesLength: number;
+  currentSlide: number;
+  setCurrentSlide: (val: number) => void;
+  setAnimate: Animate;
+}
