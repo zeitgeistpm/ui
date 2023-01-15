672eaf4ca5a424078fcbcf321f65d66314944277
robhyrk <rob.hyrkiel@gmail.com>
2023-01-14 20:31:40 -0500

    styles: update styles, add images, add box shadow to animation

--- components/front-page/LearnSection.tsx
+++ components/front-page/LearnSection.tsx
@@ -1,10 +1,12 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 interface LearnCardProps {
   tag: string;
   title: string;
   description: string;
   link: string;
+  img: string;
   className?: string;
 }
 
@@ -14,19 +16,35 @@ const LearnCard = ({
   description,
   className,
   link,
+  img,
 }: LearnCardProps) => {
   return (
     <motion.div
-      whileHover={{ scale: 1.03 }}
+      whileHover={{
+        scale: 1.03,
+        boxShadow: "0px 10px 20px 5px rgba(0,0,0,0.25)",
+      }}
       whileTap={{ scale: 1 }}
-      className={`${className} min-w-[220px] w-full h-[126px] rounded-[10px] p-[15px]`}
+      animate={{}}
+      className={`${className} w-full rounded-[10px] p-[15px] pr-[20px]`}
     >
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="bg-white py-[3px] px-[10px] rounded-[50px] w-fit text-ztg-12-120 font-bold">
+        <span className="bg-white p-[6px] rounded-[50px] w-fit text-ztg-12-120 font-bold">
           {tag}
+        </span>
+        <div className="flex justify-between gap-[30px]">
+          <div>
+            <h6 className="font-bold text-ztg-16-150 mt-[8px]">{title}</h6>
+            <p className="hidden md:block text-ztg-14-150">{description}</p>
+          </div>
+          <Image
+            src={img}
+            width={84}
+            height={80}
+            alt={title}
+            className="hidden lg:block"
+          />
         </div>
-        <div className="font-bold text-ztg-16-150 my-[8px]">{title}</div>
-        <div className="text-sky-600 text-ztg-14-150">{description}</div>
       </a>
     </motion.div>
   );
@@ -35,16 +53,14 @@ const LearnCard = ({
 const LearnSection = () => {
   return (
     <div>
-      <div className=" font-bold text-[28px] mb-[30px]">
-        Welcome to Zeitgeist
-      </div>
-      <div className="flex flex-col md:flex-row gap-[30px]">
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-[20px]">
         <LearnCard
           tag="Trade"
           title="Prediction Markets"
           description="Make money on your Beliefs"
           link="https://docs.zeitgeist.pm/docs/learn/prediction-markets"
           className="bg-tropical-blue"
+          img="/learn/learn-1.png"
         />
         <LearnCard
           tag="Earn"
@@ -52,13 +68,15 @@ const LearnSection = () => {
           description="Earn ZTG providing Liquidity"
           link="https://docs.zeitgeist.pm/docs/learn/liquidity"
           className="bg-link-water"
+          img="/learn/learn-2.png"
         />
         <LearnCard
-          tag="Create"
-          title="Create Markets"
-          description="Learn about the World"
+          tag="Native Currency"
+          title="ZTG Token"
+          description="Tokenomics and Future of ZTG"
           link="https://docs.zeitgeist.pm/docs/learn/market-rules"
-          className="bg-mystic"
+          className="bg-mystic col-span-2 sm:col-span-1 min-h-[84px]"
+          img="/learn/learn-3.png"
         />
       </div>
     </div>
