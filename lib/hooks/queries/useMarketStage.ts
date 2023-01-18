1b7590c719ab211d3699dedebc03f9e4f44002e3
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-01-18 11:02:00 +0100

    comment type docs

--- lib/hooks/queries/useMarketStage.ts
+++ lib/hooks/queries/useMarketStage.ts
@@ -3,11 +3,19 @@ import { Context, isRpcSdk, Market } from "@zeitgeistpm/sdk-next";
 import { useSdkv2 } from "../useSdkv2";
 import { marketsRootQuery } from "./useMarket";
 
+export const marketStageRootKey = "marketStage";
+
+/**
+ * Get the current stage of a market.
+ *
+ * @param market Market<Context>
+ * @returns useQuery<MarketStage, unknown, MarketStage>
+ */
 export const useMarketStage = (market?: Market<Context>) => {
   const [sdk, id] = useSdkv2();
 
   return useQuery(
-    [id, marketsRootQuery, market?.marketId, "stage"],
+    [id, marketsRootQuery, market?.marketId, "marketStageRootKey"],
     async () => {
       if (sdk && isRpcSdk(sdk) && market) {
         return await sdk.model.markets.getStage(market);
