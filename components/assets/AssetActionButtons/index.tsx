86a531e4cb24808daa1e4a9f12a285fb42971031
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-01-18 10:53:54 +0100

    use market stage to determine asset actions button layout

--- components/assets/AssetActionButtons/index.tsx
+++ components/assets/AssetActionButtons/index.tsx
@@ -1,6 +1,8 @@
 import { CategoricalAssetId, ScalarAssetId } from "@zeitgeistpm/sdk-next";
 import BuySellButtons from "components/trade-slip/BuySellButtons";
 import { useMarket } from "lib/hooks/queries/useMarket";
+import { useMarketStage } from "lib/hooks/queries/useMarketStage";
+import { useStore } from "lib/stores/Store";
 import { observer } from "mobx-react";
 import DisputeButton from "./DisputeButton";
 import RedeemButton from "./RedeemButton";
@@ -14,25 +16,49 @@ interface AssetActionButtonsProps {
 
 const AssetActionButtons = observer(
   ({ marketId, assetId, assetTicker }: AssetActionButtonsProps) => {
+    const store = useStore();
     const { data: market } = useMarket(marketId);
+    const { data: marketStage } = useMarketStage(market);
 
-    if (!market) {
+    const userAddress = store.wallets?.getActiveSigner()?.address;
+    const isOracle = market?.oracle === userAddress;
+
+    if (!market || !marketStage) {
       return null;
     }
 
-    if (market?.status === "Closed") {
-      return (
-        <ReportButton market={market} assetId={assetId} ticker={assetTicker} />
-      );
-    } else if (market?.status === "Disputed") {
+    console.log(marketStage);
+
+    if (marketStage.type === "ReportPeriod") {
+      if (
+        (marketStage.access === "oracle" && isOracle) ||
+        marketStage.access === "open"
+      ) {
+        return (
+          <ReportButton
+            market={market}
+            assetId={assetId}
+            ticker={assetTicker}
+          />
+        );
+      }
+    }
+
+    if (marketStage.type === "Disputed") {
       return null;
-    } else if (market?.status === "Reported") {
+    }
+
+    if (marketStage.type === "Reported") {
       return (
         <DisputeButton market={market} assetId={assetId} ticker={assetTicker} />
       );
-    } else if (market?.status === "Resolved") {
+    }
+
+    if (marketStage.type === "Resolved") {
       return <RedeemButton assetId={assetId} market={market} />;
-    } else {
+    }
+
+    if (marketStage.type === "Trading") {
       return <BuySellButtons assetId={assetId} disabled={assetId == null} />;
     }
   },
