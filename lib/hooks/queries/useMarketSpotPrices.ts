c821ad3bdef94460a69ef9b2545ec861d5f690c4
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-01-19 14:44:34 +0000

    add resolved market logic

--- lib/hooks/queries/useMarketSpotPrices.ts
+++ lib/hooks/queries/useMarketSpotPrices.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { isIndexedSdk, isRpcSdk } from "@zeitgeistpm/sdk-next";
 import Decimal from "decimal.js";
 import { calcSpotPrice } from "lib/math";
+import { calcScalarResolvedPrices } from "lib/util/calc-scalar-winnings";
 import { useSdkv2 } from "../useSdkv2";
 import { useAccountPoolAssetBalances } from "./useAccountPoolAssetBalances";
 import { useMarket } from "./useMarket";
@@ -17,41 +18,84 @@ export const useMarketSpotPrices = (marketId: number) => {
   const { data: market } = useMarket(marketId);
 
   const { data: balances } = useAccountPoolAssetBalances(pool?.accountId, pool);
-  console.log(balances);
 
   const query = useQuery(
     [id, assetPricesKey, pool],
     async () => {
       if (isRpcSdk(sdk)) {
-        const basePoolBalance = await sdk.context.api.query.system.account(
-          pool.accountId,
-        );
+        const spotPrices = new Map<number, Decimal>();
+        if (market.status !== "Resolved") {
+          const basePoolBalance = await sdk.context.api.query.system.account(
+            pool.accountId,
+          );
 
-        //base weight is equal to the sum of all other assets
-        const baseWeight = new Decimal(pool.totalWeight).div(2);
+          //base weight is equal to the sum of all other assets
+          const baseWeight = new Decimal(pool.totalWeight).div(2);
 
-        const spotPrices = new Map<string, Decimal>();
+          //todo: pre filter weights to reduce repetition
+          pool.weights.forEach((weight, index) => {
+            if (
+              weight.assetId.toLocaleLowerCase() !==
+              pool.baseAsset.toLocaleLowerCase()
+            ) {
+              const spotPrice = calcSpotPrice(
+                basePoolBalance.data.free.toString(),
+                baseWeight,
+                balances[index].free.toString(),
+                weight.len,
+                0,
+              );
 
-        pool.weights.forEach((weight, index) => {
-          if (
-            weight.assetId.toLocaleLowerCase() !==
-            pool.baseAsset.toLocaleLowerCase()
-          ) {
-            const spotPrice = calcSpotPrice(
-              basePoolBalance.data.free.toString(),
-              baseWeight,
-              balances[index].free.toString(),
-              weight.len,
-              0,
-            );
+              spotPrices.set(index, spotPrice);
+            }
+          });
+          return spotPrices;
+        } else {
+          console.log(market.marketType);
 
-            spotPrices.set(weight.assetId, spotPrice);
+          if (market.marketType.scalar) {
+            const { shortTokenValue, longTokenValue } =
+              calcScalarResolvedPrices(
+                new Decimal(market.marketType.scalar[0]),
+                new Decimal(market.marketType.scalar[1]),
+                new Decimal(market.resolvedOutcome),
+              );
+
+            console.log(shortTokenValue, longTokenValue);
+
+            pool.weights.forEach((weight, index) => {
+              if (
+                weight.assetId.toLocaleLowerCase() !==
+                pool.baseAsset.toLocaleLowerCase()
+              ) {
+                if (weight.assetId.includes("Short")) {
+                  spotPrices.set(index, shortTokenValue);
+                } else if (weight.assetId.includes("Long")) {
+                  spotPrices.set(index, longTokenValue);
+                }
+              }
+            });
+            return spotPrices;
+          } else {
+            pool.weights.forEach((weight, index) => {
+              if (
+                weight.assetId.toLocaleLowerCase() !==
+                pool.baseAsset.toLocaleLowerCase()
+              ) {
+                if (index === Number(market.resolvedOutcome)) {
+                  spotPrices.set(index, new Decimal(1));
+                } else {
+                  spotPrices.set(index, new Decimal(0));
+                }
+              }
+            });
+            return spotPrices;
           }
-        });
-        return spotPrices;
-      } else {
-        return null;
+        }
       }
+      // else {
+      //   return null;
+      // }
     },
     {
       enabled: Boolean(
