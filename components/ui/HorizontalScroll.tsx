1a63b52cd25d1facf5b6c4058b649bebbc3263f1
Rob Hyrkiel <rob@zeitgeist.pm>
2023-01-19 11:22:57 -0500

    532 update market cards (#589)
    * feat: create new market card file, update layout and styles
    
    * style: re-align card content,  adjust fonts and line heights for diff breakpoints
    
    * style: enable variable heights for market cards based on content, add temp files for new market scroll
    
    * refactor: create horizontal scroll ui component
    
    * style: minor style adjustments
    
    * chore: revert to previous designs temporarily
    
    * feat: replace showmarketlink check with link prop
    
    * feat: change cards shown based on new container widths
    
    * feat: calculate cards shown based on container width, update styles, adjust card width based on container width
    
    * chore: remove console log
    
    * fix: gradient overlay on scroll
    
    * fix: make width optional prop
    
    * style: change title font weight
    
    * fix: cards shown logic, add cta prop, remove unused code, remove cardwidth from useffect, adjust breakpoint for scroll, add default value for width
    
    * fix: scroll gradient z index value
    
    * style: add hover effect states for market cards
    
    * style: adjust on hover opacity

---
+++ components/ui/HorizontalScroll.tsx
@@ -0,0 +1,63 @@
+import { FC } from "react";
+import Link from "next/link";
+import { ChevronLeft } from "react-feather";
+import { ChevronRight } from "react-feather";
+
+interface HorizontalScrollProps {
+  classes?: string;
+  cta?: string;
+  link?: string;
+  handleLeftClick: () => void;
+  handleRightClick: () => void;
+  rightDisabled: boolean;
+  leftDisabled: boolean;
+}
+
+const HorizontalScroll: FC<HorizontalScrollProps> = ({
+  classes,
+  cta,
+  link,
+  handleRightClick,
+  handleLeftClick,
+  rightDisabled,
+  leftDisabled,
+}) => {
+  return (
+    <div
+      className={`flex md:ml-auto w-full items-center justify-end ${classes}`}
+    >
+      {link && (
+        <Link
+          href={link}
+          className="leading-[40px] rounded-ztg-100 text-ztg-14-150 bg-mystic px-[15px] text-center w-full sm:w-fit"
+        >
+          {cta}
+        </Link>
+      )}
+      <button
+        onClick={handleLeftClick}
+        className={`hidden md:flex items-center justify-center w-[40px] h-[40px] rounded-full ml-[12px] mr-[8px] ztg-transition ${
+          leftDisabled
+            ? "bg-sky-600 opacity-30 text-pastel-blue"
+            : "bg-pastel-blue text-white"
+        }`}
+        disabled={leftDisabled}
+      >
+        <ChevronLeft className="relative right-[1px]" />
+      </button>
+      <button
+        onClick={handleRightClick}
+        className={`hidden md:flex items-center justify-center w-[40px] h-[40px] rounded-full ztg-transition  ${
+          rightDisabled
+            ? "bg-sky-600 opacity-30 text-pastel-blue"
+            : "bg-pastel-blue text-white"
+        }`}
+        disabled={rightDisabled}
+      >
+        <ChevronRight className="relative left-[1px]" />
+      </button>
+    </div>
+  );
+};
+
+export default HorizontalScroll;
