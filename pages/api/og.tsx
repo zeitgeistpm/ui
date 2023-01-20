522219a80adc65874eff5784dbc4c0455a23ee55
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-01-20 12:01:23 +0100

    work on rendering market images

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,11 +1,31 @@
 import { ImageResponse } from "@vercel/og";
-import { GraphQLClient } from "graphql-request";
+import { gql, GraphQLClient } from "graphql-request";
 import { getMarket } from "lib/gql/markets";
+import { create } from "@zeitgeistpm/sdk-next";
+import { isMarketImageBase64Encoded } from "lib/types/create-market";
+import { getCurrentPrediction } from "lib/util/assets";
+import moment from "moment";
 import { NextApiRequest, NextApiResponse } from "next";
+import Decimal from "decimal.js";
+import { ZTG } from "lib/constants";
 
 export const config = {
   runtime: "experimental-edge",
 };
+
+const sdkPromise = create({
+  indexer: process.env.NEXT_PUBLIC_SSR_INDEXER_URL,
+});
+
+const boldFont = fetch(
+  new URL("../../public/fonts/inter/static/Inter-Bold.ttf", import.meta.url)
+    .href,
+).then((res) => res.arrayBuffer());
+
+const regularFont = fetch(
+  new URL("../../public/fonts/inter/static/Inter-Regular.ttf", import.meta.url)
+    .href,
+).then((res) => res.arrayBuffer());
 
 export default async function (
   request: NextApiRequest,
@@ -21,11 +41,20 @@ export default async function (
 
   const marketId = searchParams.get("marketId");
 
+  const sdk = await sdkPromise;
+
   const client = new GraphQLClient(process.env.NEXT_PUBLIC_SSR_INDEXER_URL, {
     fetch,
   });
 
-  const market = await getMarket(client, marketId as string);
+  // const market = await getMarket(client, marketId as string);
+  const { markets } = await sdk.context.indexer.markets({
+    where: {
+      marketId_eq: Number(marketId),
+    },
+  });
+
+  const market = markets[0];
 
   if (!market) {
     return new Response(`no market found by id ${marketId}`, {
@@ -33,24 +62,136 @@ export default async function (
     });
   }
 
+  let prediction: string = "No Prediction";
+
+  if (market.pool) {
+    const assetsRes = await client.request<{
+      assets: {
+        poolId: number;
+        price: number;
+      }[];
+    }>(
+      gql`
+        query Assets($poolId: Int) {
+          assets(where: { poolId_eq: $poolId }) {
+            poolId
+            price
+            assetId
+          }
+        }
+      `,
+      {
+        poolId: market.pool.poolId,
+      },
+    );
+
+    prediction = getCurrentPrediction(assetsRes.assets, market);
+  } else {
+    prediction = "No Prediction";
+  }
+
+  const marketImage = !market.img
+    ? new URL("../../public/icons/default-market.png", import.meta.url).href
+    : isMarketImageBase64Encoded(market.img)
+    ? market.img
+    : `https://ipfs-gateway.zeitgeist.pm/ipfs/${market.img}`;
+
+  const boldFontData = await boldFont;
+  const regularFontData = await regularFont;
+
   const image = (
     <div
+      tw="p-12 text-white"
       style={{
-        background: "white",
         width: "100%",
         height: "100%",
         display: "flex",
-        textAlign: "center",
-        alignItems: "center",
-        justifyContent: "center",
       }}
     >
-      <div tw="bg-gray-50">{market.status}</div>
+      <img
+        src={new URL("../../public/og/bg1.png", import.meta.url).href}
+        tw="absolute top-0 left-0"
+        style={{
+          width: 1200,
+          height: 675,
+          objectFit: "cover",
+        }}
+      />
+
+      <div tw="flex flex-col h-full mr-20">
+        <div tw="flex flex-1">
+          <img
+            style={{
+              width: 140,
+              height: 140,
+              objectFit: "cover",
+              borderRadius: "100%",
+            }}
+            src={marketImage}
+          />
+        </div>
+        <div tw="flex flex-col">
+          <h2 tw="font-bold text-3xl font-sans">Ends:</h2>
+          <div tw="text-2xl -mt-3" style={{ color: "#ABC1F9" }}>
+            {moment(Number(market.period.end)).format("MMM Do, YYYY")}
+          </div>
+        </div>
+      </div>
+
+      <div tw="flex flex-1 pr-30 flex-col h-full">
+        <h1 tw="text-5xl mb-20" style={{ lineHeight: "1.3em" }}>
+          {market.question}
+        </h1>
+
+        <div tw="flex flex-col flex-1">
+          <h2 tw="font-bold text-4xl font-sans">
+            {market.status === "Reported" || market.status === "Resolved"
+              ? "Winning Outcome:"
+              : "Prediction:"}
+          </h2>
+          <div tw="text-3xl -mt-3" style={{ color: "#ABC1F9" }}>
+            {prediction}
+          </div>
+        </div>
+
+        <div tw="flex flex-col">
+          <h2 tw="font-bold text-3xl font-sans">Volume:</h2>
+          <div tw="flex text-2xl -mt-3" style={{ color: "#ABC1F9" }}>
+            {new Decimal(market.pool?.volume).div(ZTG).toFixed(2)}
+            {" ZTG"}
+          </div>
+        </div>
+      </div>
+
+      <img
+        tw="absolute bottom-12 right-12"
+        style={{
+          transform: "scale(0.5)",
+          transformOrigin: "bottom right",
+        }}
+        src={
+          new URL("../../public/og/zeitgeist_badge.png", import.meta.url).href
+        }
+      />
     </div>
   );
 
   return new ImageResponse(image, {
     width: 1200,
-    height: 630,
+    height: 675,
+    fonts: [
+      {
+        name: "Inter",
+        data: regularFontData,
+        weight: 400,
+        style: "normal",
+      },
+      {
+        name: "Inter",
+        data: boldFontData,
+        weight: 700,
+        style: "normal",
+      },
+    ],
   });
 }
