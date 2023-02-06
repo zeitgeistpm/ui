179d163fc93d1bdc63c9da560765045c85793a1f
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-02-06 16:54:29 +0100

    fix keys and props for meta tags

--- components/og/OgHead.tsx
+++ components/og/OgHead.tsx
@@ -12,21 +12,41 @@ export const OgHead = (props: OgProps) => {
       {props.title && (
         <>
           <title>{props.title}</title>
-          <meta property="og:title" content={props.title} key="title" />
-          <meta property="twitter:title" content={props.title} key="title" />
+          <meta key="og:title" property="og:title" content={props.title} />
+          <meta
+            key="twitter:title"
+            property="twitter:title"
+            content={props.title}
+          />
         </>
       )}
       {props.description && (
         <>
-          <meta name="description" content={props.description} />
-          <meta property="og:description" content={props.description} />
-          <meta property="twitter:description" content={props.description} />
+          <meta
+            key="description"
+            name="description"
+            content={props.description}
+          />
+          <meta
+            key="og:description"
+            property="og:description"
+            content={props.description}
+          />
+          <meta
+            key="twitter:description"
+            property="twitter:description"
+            content={props.description}
+          />
         </>
       )}
       {props.image && (
         <>
           <meta key="og:image" property="og:image" content={props.image} />
-          <meta key="twitter:image" property="og:image" content={props.image} />
+          <meta
+            key="twitter:image"
+            property="twitter:image"
+            content={props.image}
+          />
         </>
       )}
     </Head>
