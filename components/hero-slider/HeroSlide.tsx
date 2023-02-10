8efffc8d595f58f8859f1f1deccdd77c35797290
robhyrk <rob.hyrkiel@gmail.com>
2023-02-10 00:05:39 -0500

    fix: hydration issue with links embedded inside Link in hero slider

--- components/hero-slider/HeroSlide.tsx
+++ components/hero-slider/HeroSlide.tsx
@@ -5,7 +5,7 @@ import { HeroSlideProps } from "./slider-types";
 
 export const HeroSlide: FC<HeroSlideProps> = ({ slide }) => {
   return (
-    <Link href={!slide.title.text && slide.link1}>
+    <Link href={!slide.title.text ? slide.link1 : "/"}>
       <div className="flex items-center h-full w-full">
         <div className="w-full md:max-w-[540px] pb-8">
           <h2
@@ -13,34 +13,36 @@ export const HeroSlide: FC<HeroSlideProps> = ({ slide }) => {
           >
             {slide.title.text}
           </h2>
-          <div className="flex flex-col sm:flex-row">
-            <a
-              style={{
-                borderColor: `${slide.color1.border}`,
-                backgroundColor: `${slide.color1.primary}`,
-                color: `${slide.color1.secondary}`,
-              }}
-              className="leading-[42px] w-full sm:w-fit text-center sm:text-start border rounded px-5 mb-5 mr-5 font-bold"
-              href={slide.link1}
-              target="_blank"
-            >
-              {slide.cta1}
-            </a>
-            {slide.cta2 && (
+          {slide.title.text && (
+            <div className="flex flex-col sm:flex-row">
               <a
                 style={{
-                  borderColor: `${slide.color2.border}`,
-                  backgroundColor: `${slide.color2.primary}`,
-                  color: `${slide.color2.secondary}`,
+                  borderColor: `${slide.color1.border}`,
+                  backgroundColor: `${slide.color1.primary}`,
+                  color: `${slide.color1.secondary}`,
                 }}
-                className="leading-[42px] w-full sm:w-fit text-center sm:text-start border rounded px-5 mb-5 font-bold"
-                href={slide.link2}
+                className="leading-[42px] w-full sm:w-fit text-center sm:text-start border rounded px-5 mb-5 mr-5 font-bold"
+                href={slide.link1}
                 target="_blank"
               >
-                {slide.cta2}
+                {slide.cta1}
               </a>
-            )}
-          </div>
+              {slide.cta2 && (
+                <a
+                  style={{
+                    borderColor: `${slide.color2.border}`,
+                    backgroundColor: `${slide.color2.primary}`,
+                    color: `${slide.color2.secondary}`,
+                  }}
+                  className="leading-[42px] w-full sm:w-fit text-center sm:text-start border rounded px-5 mb-5 font-bold"
+                  href={slide.link2}
+                  target="_blank"
+                >
+                  {slide.cta2}
+                </a>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </Link>
