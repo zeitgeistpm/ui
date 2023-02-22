017626316c5bb599b4c2aa21e365151d8eb3bcef
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-02-22 16:24:06 +0000

    refactor and add wallets selection

--- components/account/OnboardingModal.tsx
+++ components/account/OnboardingModal.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Stepper = ({ steps, currentStep }) => {
   return (
     <div className="flex gap-x-[15px]">
@@ -14,6 +16,86 @@ const Stepper = ({ steps, currentStep }) => {
   );
 };
 
+interface TextSectionProps {
+  headerText: string;
+  bodyText: string;
+  leftButtonText: string;
+  rightButtonText: string;
+  onLeftButtonClick: () => void;
+  onRightButtonClick: () => void;
+}
+const TextSection = ({
+  headerText,
+  bodyText,
+  leftButtonText,
+  rightButtonText,
+  onLeftButtonClick,
+  onRightButtonClick,
+}: TextSectionProps) => {
+  return (
+    <>
+      <div className="font-bold text-ztg-22-120">{headerText}</div>
+      <div className="text-center">{bodyText}</div>
+      <div className="flex justify-center  gap-x-[20px] w-full px-[20px] h-[56px] font-medium">
+        <button
+          className="rounded-[100px] border-2 border-pastel-blue w-full"
+          onClick={onLeftButtonClick}
+        >
+          {leftButtonText}
+        </button>
+        <button
+          className="rounded-[100px] border-2 border-pastel-blue w-full"
+          onClick={onRightButtonClick}
+        >
+          {rightButtonText}
+        </button>
+      </div>
+    </>
+  );
+};
+
+const WalletSelection = () => {
+  const wallets = [
+    {
+      name: "Talisman",
+      image: "talisman.png",
+      recommended: true,
+    },
+    {
+      name: "Polkadot.js",
+      image: "Polkadot-js.png",
+      recommended: false,
+    },
+    {
+      name: "Subwallet",
+      image: "subwallet.png",
+      recommended: false,
+    },
+  ];
+
+  return (
+    <>
+      {wallets.map((wallet, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-center h-[56px] border-2 border-pastel-blue rounded-ztg-10 text-center w-full"
+        >
+          <Image
+            src={`/icons/${wallet.image}`}
+            alt={wallet.name}
+            width={30}
+            height={30}
+          />
+          <div className="font-medium text-ztg-18-150 ml-[15px]">
+            {wallet.name}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+const ExchangeSelection = () => {};
+
 const OnBoardingModal = () => {
   return (
     <div
@@ -21,20 +103,18 @@ const OnBoardingModal = () => {
                 border-black h-[438px] w-full max-w-[526px] p-[30px] rounded-ztg-10"
     >
       <div className="bg-ztg-blue rounded-full w-[120px] h-[120px]"></div>
-      <div className="font-bold text-ztg-22-120">Getting Started</div>
-      <div className="text-center">
-        First thing you need to do is to Install the right extension for your
-        account. To do that, you need to click the wallet icon to go to its
-        download page (e.g. Talisman).
-      </div>
-      <div className="flex justify-center  gap-x-[20px] w-full px-[20px] h-[56px] font-medium">
-        <button className="rounded-[100px] border-2 border-pastel-blue w-full">
-          Back
-        </button>
-        <button className="rounded-[100px] border-2 border-pastel-blue w-full">
-          Continue
-        </button>
-      </div>
+      {/* <Image
+            alt=
+            src={imageUrl}
+            fill
+            className="rounded-full"
+            style={{
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+            sizes={size}
+          /> */}
+      <WalletSelection />
       <Stepper steps={5} currentStep={1} />
     </div>
   );
