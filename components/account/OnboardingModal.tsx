a5479c130cd3e029d05457e40e73928fc786fd5d
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-02-23 11:03:26 +0000

    add all steps

--- components/account/OnboardingModal.tsx
+++ components/account/OnboardingModal.tsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
+import { useState } from "react";
 
-const Stepper = ({ steps, currentStep }) => {
+interface StepperProps {
+  steps: number;
+  currentStep: number;
+  onStepClick?: (step: number) => void;
+}
+
+const Stepper = ({ steps, currentStep, onStepClick }: StepperProps) => {
   return (
-    <div className="flex gap-x-[15px]">
+    <div className="flex gap-x-[18px]">
       {Array(steps)
         .fill(null)
         .map((_, index) => (
-          <div
-            className={`rounded-full h-[6px] w-[6px] ${
+          <button
+            onClick={() => onStepClick(index)}
+            disabled={index === currentStep}
+            className={`rounded-full h-[7px] w-[7px] ${
               index === currentStep ? "bg-black" : "bg-sky-600"
             }`}
-          ></div>
+          ></button>
         ))}
     </div>
   );
@@ -19,10 +28,10 @@ const Stepper = ({ steps, currentStep }) => {
 interface TextSectionProps {
   headerText: string;
   bodyText: string;
-  leftButtonText: string;
-  rightButtonText: string;
-  onLeftButtonClick: () => void;
-  onRightButtonClick: () => void;
+  leftButtonText?: string;
+  rightButtonText?: string;
+  onLeftButtonClick?: () => void;
+  onRightButtonClick?: () => void;
 }
 const TextSection = ({
   headerText,
@@ -35,20 +44,24 @@ const TextSection = ({
   return (
     <>
       <div className="font-bold text-ztg-22-120">{headerText}</div>
-      <div className="text-center">{bodyText}</div>
+      <div className="text-center mb-auto">{bodyText}</div>
       <div className="flex justify-center  gap-x-[20px] w-full px-[20px] h-[56px] font-medium">
-        <button
-          className="rounded-[100px] border-2 border-pastel-blue w-full"
-          onClick={onLeftButtonClick}
-        >
-          {leftButtonText}
-        </button>
-        <button
-          className="rounded-[100px] border-2 border-pastel-blue w-full"
-          onClick={onRightButtonClick}
-        >
-          {rightButtonText}
-        </button>
+        {leftButtonText && (
+          <button
+            className="rounded-[100px] border-2 border-pastel-blue w-full"
+            onClick={onLeftButtonClick}
+          >
+            {leftButtonText}
+          </button>
+        )}
+        {rightButtonText && (
+          <button
+            className="rounded-[100px] border-2 border-pastel-blue w-full"
+            onClick={onRightButtonClick}
+          >
+            {rightButtonText}
+          </button>
+        )}
       </div>
     </>
   );
@@ -133,12 +146,13 @@ const ExchangeTypeSelection = () => {
 };
 
 const OnBoardingModal = () => {
+  const [step, setStep] = useState(0);
   return (
     <div
       className="flex flex-col gap-y-[20px] justify-center items-center bg-white border 
                 border-black h-[438px] w-full max-w-[526px] p-[30px] rounded-ztg-10"
     >
-      <div className="rounded-full w-[120px] h-[120px]">
+      <div className="rounded-full w-[120px] h-[120px] mb-auto">
         <Image
           alt="AI Logan?"
           src={"/misc/face.png"}
@@ -146,9 +160,47 @@ const OnBoardingModal = () => {
           height={120}
         />
       </div>
-      {/* <ExchangeTypeSelection /> */}
-      <WalletSelection />
-      <Stepper steps={5} currentStep={1} />
+      {step === 0 && (
+        <TextSection
+          headerText="Welcome to Zeitgeist"
+          bodyText="Hey, it looks like you don’t have any wallets installed. Let me be your Guide and help you get one to start using the App to its full extent"
+          rightButtonText="Continue"
+          onRightButtonClick={() => setStep(1)}
+        />
+      )}
+      {step === 1 && (
+        <TextSection
+          headerText="Getting Started"
+          bodyText="First thing you need to do is to Install the right extension for your account. To do that, you need to click the wallet icon to go to its download page (e.g. Talisman)."
+          leftButtonText="Back"
+          rightButtonText="Continue"
+          onLeftButtonClick={() => setStep(0)}
+          onRightButtonClick={() => setStep(2)}
+        />
+      )}
+      {step === 2 && (
+        <TextSection
+          headerText="Getting Your Wallet"
+          bodyText="Finally, you’re just about to become a proud member of Zeitgeist. And we hope you’ll have a nice ride and unforgettable experience with Prediction Markets."
+          leftButtonText="Back"
+          rightButtonText="Continue"
+          onLeftButtonClick={() => setStep(1)}
+          onRightButtonClick={() => setStep(3)}
+        />
+      )}
+      {step === 3 && <WalletSelection />}
+      {step === 4 && (
+        <TextSection
+          headerText="Success on getting a wallet!"
+          bodyText="Now to get ZTG."
+          leftButtonText="Back"
+          rightButtonText="Continue"
+          onLeftButtonClick={() => setStep(3)}
+          onRightButtonClick={() => setStep(5)}
+        />
+      )}
+      {step === 5 && <ExchangeTypeSelection />}
+      <Stepper steps={6} currentStep={step} onStepClick={setStep} />
     </div>
   );
 };
