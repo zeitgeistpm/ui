96c96d29b3741c1a0429ed9021903dbf1e10c783
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-02-24 15:40:49 +0000

    Replace created at with starts. Move component out to avoid rerenders

--- pages/markets/[marketid].tsx
+++ pages/markets/[marketid].tsx
@@ -111,6 +111,103 @@ export async function getStaticProps({ params }) {
   };
 }
 
+const HeaderStat: FC<{ label: string; border?: boolean }> = ({
+  label,
+  border = true,
+  children,
+}) => {
+  return (
+    <div className={border ? "sm:border-r sm:border-ztg-blue pr-2" : ""}>
+      <span>{label}: </span>
+      <span className="font-medium">{children}</span>
+    </div>
+  );
+};
+
+const Tag: FC<{ className?: string }> = ({ className, children }) => {
+  return (
+    <span className={`px-2.5 py-1 rounded bg-gray-300 ${className}`}>
+      {children}
+    </span>
+  );
+};
+
+const MarketHeader: FC<{
+  question: string;
+  status: string;
+  tags: string[];
+  starts: number;
+  ends: number;
+  prizePool: number;
+  subsidy: number;
+  volume: number;
+  token: string;
+}> = ({
+  question,
+  status,
+  tags,
+  starts,
+  ends,
+  prizePool,
+  subsidy,
+  volume,
+  token,
+}) => {
+  return (
+    <header className="text-center">
+      <h1 className="font-bold text-4xl my-5">{question}</h1>
+      <div className="flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-2 mb-5">
+        <HeaderStat label={hasDatePassed(starts) ? "Started" : "Starts"}>
+          {new Intl.DateTimeFormat("default", {
+            dateStyle: "medium",
+          }).format(starts)}
+        </HeaderStat>
+        <HeaderStat label={hasDatePassed(starts) ? "Ended" : "Ends"}>
+          {new Intl.DateTimeFormat("default", {
+            dateStyle: "medium",
+          }).format(ends)}
+        </HeaderStat>
+        {token ? (
+          <HeaderStat label="Volume">
+            {formatNumberCompact(volume)}
+            &nbsp;
+            {token}
+          </HeaderStat>
+        ) : (
+          <Skeleton width="150px" height="24px" />
+        )}
+        {prizePool >= 0 && token ? (
+          <HeaderStat label="Prize Pool">
+            {formatNumberCompact(prizePool)}
+            &nbsp;
+            {token}
+          </HeaderStat>
+        ) : (
+          <Skeleton width="150px" height="24px" />
+        )}
+        {subsidy >= 0 && token ? (
+          <HeaderStat label="Subsidy" border={false}>
+            {formatNumberCompact(subsidy)}
+            &nbsp;
+            {token}
+          </HeaderStat>
+        ) : (
+          <Skeleton width="150px" height="24px" />
+        )}
+      </div>
+      <div className="flex flex-wrap justify-center gap-2.5">
+        <Tag className={`${status === "Active" && "!bg-green-lighter"}`}>
+          {status === "Active" && <span className="text-green">&#x2713; </span>}
+          {status}
+        </Tag>
+        {tags?.map((tag, index) => {
+          return <Tag key={index}>{tag}</Tag>;
+        })}
+      </div>
+    </header>
+  );
+};
+
 const Market: NextPage<{
   indexedMarket: MarketPageIndexedData;
   chartSeries: ChartSeries[];
@@ -165,110 +262,9 @@ const Market: NextPage<{
   //required to fix title element warning
   const question = indexedMarket.question;
 
-  const HeaderStat: FC<{ label: string; border?: boolean }> = ({
-    label,
-    border = true,
-    children,
-  }) => {
-    return (
-      <div className={border ? "sm:border-r sm:border-ztg-blue pr-2" : ""}>
-        <span>{label}: </span>
-        <span className="font-medium">{children}</span>
-      </div>
-    );
-  };
-
-  const Tag: FC<{ className?: string }> = ({ className, children }) => {
-    return (
-      <span className={`px-2.5 py-1 rounded bg-gray-300 ${className}`}>
-        {children}
-      </span>
-    );
-  };
-
-  const MarketHeader: FC<{
-    question: string;
-    status: string;
-    tags: string[];
-    createdAt: number;
-    ends: number;
-    prizePool: number;
-    subsidy: number;
-    volume: number;
-    token: string;
-  }> = ({
-    question,
-    status,
-    tags,
-    createdAt,
-    ends,
-    prizePool,
-    subsidy,
-    volume,
-    token,
-  }) => {
-    return (
-      <header className="text-center">
-        <h1 className="font-bold text-4xl my-5">{question}</h1>
-        <div className="flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-2 mb-5">
-          <HeaderStat label="Created">
-            {new Intl.DateTimeFormat("default", {
-              dateStyle: "medium",
-            }).format(createdAt)}
-          </HeaderStat>
-          <HeaderStat label={hasDatePassed(ends) ? "Ended" : "Ends"}>
-            {new Intl.DateTimeFormat("default", {
-              dateStyle: "medium",
-            }).format(ends)}
-          </HeaderStat>
-          {token ? (
-            <HeaderStat label="Volume">
-              {formatNumberCompact(volume)}
-              &nbsp;
-              {token}
-            </HeaderStat>
-          ) : (
-            <Skeleton width="150px" height="24px" />
-          )}
-          {prizePool >= 0 && token ? (
-            <HeaderStat label="Prize Pool">
-              {formatNumberCompact(prizePool)}
-              &nbsp;
-              {token}
-            </HeaderStat>
-          ) : (
-            <Skeleton width="150px" height="24px" />
-          )}
-          {subsidy >= 0 && token ? (
-            <HeaderStat label="Subsidy" border={false}>
-              {formatNumberCompact(subsidy)}
-              &nbsp;
-              {token}
-            </HeaderStat>
-          ) : (
-            <Skeleton width="150px" height="24px" />
-          )}
-        </div>
-        <div className="flex flex-wrap justify-center gap-2.5">
-          <Tag className={`${status === "Active" && "!bg-green-lighter"}`}>
-            {status === "Active" && (
-              <span className="text-green">&#x2713; </span>
-            )}
-            {status}
-          </Tag>
-          {tags?.map((tag, index) => {
-            return <Tag key={index}>{tag}</Tag>;
-          })}
-        </div>
-      </header>
-    );
-  };
-
   //data for MarketHeader
   const token = store?.config?.tokenSymbol;
-  const createdAt = indexedMarket?.pool?.createdAt
-    ? new Date(indexedMarket.pool.createdAt).getTime()
-    : Number(indexedMarket.period.start);
+  const starts = Number(indexedMarket.period.start);
   const ends = Number(indexedMarket.period.end);
   const volume = indexedMarket?.pool?.volume
     ? new Decimal(indexedMarket?.pool?.volume).div(ZTG).toNumber()
@@ -290,7 +286,7 @@ const Market: NextPage<{
           question={question}
           status={indexedMarket.status}
           tags={indexedMarket.tags}
-          createdAt={createdAt}
+          starts={starts}
           ends={ends}
           token={token}
           prizePool={prizePool}
