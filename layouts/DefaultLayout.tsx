ac1404590791ae89c259a233c983cfd61b61f368
robhyrk <rob.hyrkiel@gmail.com>
2023-02-28 09:10:57 -0500

    chore: code cleanup

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -72,7 +72,7 @@ const DefaultLayout: FC = observer(({ children }) => {
           className="overflow-y-a1uto overflow-x-hidden flex-grow"
         >
           <TopBar />
-          {/* hide notification bar */}
+          {/* //hide navbar until designs are ready */}
           {NOTIFICATION_MESSAGE && (
             <div className="sticky top-ztg-76 z-ztg-2 flex w-full justify-center items-center bg-yellow-100 h-ztg-38 hidden">
               <div className="text-ztg-12-150 font-semibold">
@@ -81,8 +81,8 @@ const DefaultLayout: FC = observer(({ children }) => {
             </div>
           )}
           <main
-            className={`flex flex-col dark:text-white ${
-              router.pathname !== "/" && "main-container pt-20"
+            className={`flex flex-col dark:text-white mb-12 ${
+              router.pathname !== "/" && "main-container mt-32"
             }`}
             ref={mainRef}
           >
