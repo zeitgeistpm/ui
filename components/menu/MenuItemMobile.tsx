12e93fde27c965141149b08abd498814497f3bf0
robhyrk <rob.hyrkiel@gmail.com>
2023-03-03 20:13:14 -0500

    style: adjust mobile styles

--- components/menu/MenuItemMobile.tsx
+++ components/menu/MenuItemMobile.tsx
@@ -5,7 +5,6 @@ import React, { FC } from "react";
 import { Icon } from "react-feather";
 
 export interface MenuItemMobileProps {
-  IconComponent: Icon;
   textLabel?: string;
   className?: string;
   href?: string;
@@ -19,28 +18,15 @@ const WrapComponent: FC<{ href: string }> = ({ children, href }) => {
 };
 
 export const MenuItemMobile: FC<MenuItemMobileProps> = observer(
-  ({
-    IconComponent,
-    textLabel,
-    className = "",
-    href,
-    active = false,
-    onClick,
-  }) => {
+  ({ textLabel, className = "", href, active = false, onClick }) => {
     const { pathname } = useRouter();
     active = pathname === "/" ? false : active;
 
     return (
       <WrapComponent href={href}>
-        <button
-          className={`group rounded-full p-5 w-[68px] h-[68px] bg-black border-2 border-black text-white hover:border-sunglow-2 hover:text-sunglow-2 hover:bg-black transition-all duration-300 focus:bg-ztg-blue focus:text-white focus:border-ztg-blue ${
-            active && "bg-ztg-blue border-ztg-blue"
-          } ${className}`}
-          onClick={onClick}
-        >
+        <button className={`${className}`} onClick={onClick}>
           <div className="flex flex-col center">
-            <IconComponent size={24} />
-            <div className="mt-7 whitespace-nowrap text-black text-lg">
+            <div className="whitespace-nowrap text-black text-lg">
               {textLabel}
             </div>
           </div>
--- components/menu/MobileMenu.tsx
+++ components/menu/MobileMenu.tsx
@@ -2,37 +2,7 @@ import { motion } from "framer-motion";
 import { useNavigationStore } from "lib/stores/NavigationStore";
 import { MenuItemMobile } from "./MenuItemMobile";
 import { PageName } from "lib/types/navigation";
-import { useStore } from "lib/stores/Store";
 import { observer } from "mobx-react";
-import Link from "next/link";
-import ThemeSwitch from "./ThemeSwitch";
-
-// const MobileLink = observer(
-//   ({ title, link, pageName, IconComponent, active }) => {
-//     const store = useStore();
-//     const navigationStore = useNavigationStore();
-
-//     const handleClick = () => {
-//       store.toggleShowMobileMenu();
-//       navigationStore.setPage(pageName);
-//     };
-//     return (
-//       <Link href={link}>
-//         <div className="flex flex-col items-center" onClick={handleClick}>
-//           <div className="">
-//             <IconComponent
-//               size={24}
-//               className={`p-ztg-2 rounded-ztg-5 ${
-//                 active ? "bg-ztg-blue text-white" : "dark:text-white"
-//               }`}
-//             />
-//           </div>
-//           <div className="mt-ztg-10 dark:text-white">{title}</div>
-//         </div>
-//       </Link>
-//     );
-//   },
-// );
 
 const MobileMenu = observer(() => {
   const navigationStore = useNavigationStore();
@@ -49,9 +19,9 @@ const MobileMenu = observer(() => {
       exit={{ y: -760 }}
       animate={{ y: 0 }}
       transition={{ type: "tween", duration: 0.2 }}
-      className="fixed top-0 left-0 w-full h-full z-30 bg-whiteflex flex-col items-center pt-[150px] bg-white text-black"
+      className="fixed top-0 left-0 w-full h-full z-30 flex flex-col items-center pt-[125px] bg-white text-black"
     >
-      <div className="flex flex-col items-center gap-14">
+      <div className="flex flex-col justify-between items-center gap-7">
         {Object.keys(navigationStore.items)
           .filter((itemKey) => {
             // Skip court page for now...
@@ -72,7 +42,6 @@ const MobileMenu = observer(() => {
             return (
               <MenuItemMobile
                 href={item.href}
-                IconComponent={item.IconComponent}
                 textLabel={item.label}
                 active={navigationStore.checkPage(itemKey as any)}
                 className=""
@@ -82,8 +51,7 @@ const MobileMenu = observer(() => {
             );
           })}
       </div>
-      {/* <ThemeSwitch className="mt-auto mb-ztg-24" /> */}
-      <div className="text-ztg-14-150 mb-ztg-20 mx-ztg-20 text-center dark:text-white">
+      <div className="p-7 text-center text-xs">
         Trading is only available on the desktop version of the application.
       </div>
     </motion.div>
