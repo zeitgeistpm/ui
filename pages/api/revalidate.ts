33ba218d2d45583c030b4c032a925988f43ad5a1
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-03-06 15:22:37 +0100

    log error

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,18 +4,10 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-  console.log(process.env.REVALIDATION_TOKEN);
-  // Check for secret to confirm this is a valid request
   if (request.query.token !== process.env.REVALIDATION_TOKEN) {
     return response.status(401).json({ message: "Invalid token" });
   }
 
-  try {
-    await response.revalidate("/");
-    return response.json({ success: true, revalidated: true });
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return response.status(500).send("Error revalidating");
-  }
+  await response.revalidate("/");
+  return response.json({ success: true, revalidated: true });
 }
