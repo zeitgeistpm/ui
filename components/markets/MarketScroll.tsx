510e84443c2a618f8c7c79efbc2026f9035b2ff0
robhyrk <rob.hyrkiel@gmail.com>
2023-03-15 17:57:05 -0400

    style: hide gradient until md breakpoint

--- components/markets/MarketScroll.tsx
+++ components/markets/MarketScroll.tsx
@@ -85,9 +85,9 @@ const MarketScroll = observer(
         <div className="col-span-3 relative">
           {(scrollDirection === "left" && scrollLeft !== 0) ||
           (scrollDirection === "right" && hasReachedEnd) ? (
-            <div className="bg-gradient-to-r from-white w-[20px] absolute z-ztg-10 -left-[5px] h-full"></div>
+            <div className="md:bg-gradient-to-r from-white w-[20px] absolute z-ztg-10 -left-[5px] h-full"></div>
           ) : (
-            <div className="bg-gradient-to-r from-transparent to-white w-[20px] absolute z-ztg-1 -right-[5px] h-full"></div>
+            <div className="md:bg-gradient-to-r from-transparent to-white w-[20px] absolute z-ztg-1 -right-[5px] h-full"></div>
           )}
           <div
             ref={scrollRef}
