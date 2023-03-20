166e5ea5b12cc94e529b8f79445e6693784d0e99
robhyrk <rob.hyrkiel@gmail.com>
2023-03-19 23:24:05 -0400

    feat: add optional size options for each breakpoint

--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -6,7 +6,6 @@ type fontSize =
   | 16
   | 18
   | 20
-  | 22
   | 24
   | 28
   | 30
@@ -19,6 +18,8 @@ type fontSize =
 
 interface HeadingProps {
   as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  size?: fontSize;
+  xxl?: fontSize;
   xl?: fontSize;
   lg?: fontSize;
   md?: fontSize;
@@ -28,6 +29,8 @@ interface HeadingProps {
 
 const Heading = ({
   as: heading,
+  size,
+  xxl,
   xl,
   lg,
   md,
@@ -35,8 +38,177 @@ const Heading = ({
   className = "",
   children,
 }: PropsWithChildren<HeadingProps>) => {
-  const getPrefix = (size: fontSize, prefix: string) => {
-    return `${prefix}${getSize(size)}`;
+  const getSmallSize = (size: fontSize) => {
+    switch (size) {
+      case 12:
+        return "sm:text-xs";
+      case 14:
+        return "sm:text-sm";
+      case 16:
+        return "sm:text-base";
+      case 18:
+        return "sm:text-lg";
+      case 20:
+        return "sm:text-xl";
+      case 24:
+        return "sm:text-2xl";
+      case 28:
+        return "sm:text-2.5xl";
+      case 30:
+        return "sm:text-3xl";
+      case 36:
+        return "sm:text-4xl";
+      case 48:
+        return "sm:text-5xl";
+      case 60:
+        return "sm:text-6xl";
+      case 72:
+        return "sm:text-7xl";
+      case 96:
+        return "sm:text-8xl";
+      case 128:
+        return "sm:text-9xl";
+      default:
+        return "";
+    }
+  };
+  const getMediumSize = (size: fontSize) => {
+    switch (size) {
+      case 12:
+        return "md:text-xs";
+      case 14:
+        return "md:text-sm";
+      case 16:
+        return "md:text-base";
+      case 18:
+        return "md:text-lg";
+      case 20:
+        return "md:text-xl";
+      case 24:
+        return "md:text-2xl";
+      case 28:
+        return "md:text-2.5xl";
+      case 30:
+        return "md:text-3xl";
+      case 36:
+        return "md:text-4xl";
+      case 48:
+        return "md:text-5xl";
+      case 60:
+        return "md:text-6xl";
+      case 72:
+        return "md:text-7xl";
+      case 96:
+        return "md:text-8xl";
+      case 128:
+        return "md:text-9xl";
+      default:
+        return "";
+    }
+  };
+  const getLargeSize = (size: fontSize) => {
+    switch (size) {
+      case 12:
+        return "lg:text-xs";
+      case 14:
+        return "lg:text-sm";
+      case 16:
+        return "lg:text-base";
+      case 18:
+        return "lg:text-lg";
+      case 20:
+        return "lg:text-xl";
+      case 24:
+        return "lg:text-2xl";
+      case 28:
+        return "lg:text-2.5xl";
+      case 30:
+        return "lg:text-3xl";
+      case 36:
+        return "lg:text-4xl";
+      case 48:
+        return "lg:text-5xl";
+      case 60:
+        return "lg:text-6xl";
+      case 72:
+        return "lg:text-7xl";
+      case 96:
+        return "lg:text-8xl";
+      case 128:
+        return "lg:text-9xl";
+      default:
+        return "";
+    }
+  };
+
+  const getXLSize = (size: fontSize) => {
+    switch (size) {
+      case 12:
+        return "xl:text-xs";
+      case 14:
+        return "xl:text-sm";
+      case 16:
+        return "xl:text-base";
+      case 18:
+        return "xl:text-lg";
+      case 20:
+        return "xl:text-xl";
+      case 24:
+        return "xl:text-2xl";
+      case 28:
+        return "xl:text-2.5xl";
+      case 30:
+        return "xl:text-3xl";
+      case 36:
+        return "xl:text-4xl";
+      case 48:
+        return "xl:text-5xl";
+      case 60:
+        return "xl:text-6xl";
+      case 72:
+        return "xl:text-7xl";
+      case 96:
+        return "xl:text-8xl";
+      case 128:
+        return "xl:text-9xl";
+      default:
+        return "";
+    }
+  };
+
+  const get2XLSize = (size: fontSize) => {
+    switch (size) {
+      case 12:
+        return "2xl:text-xs";
+      case 14:
+        return "2xl:text-sm";
+      case 16:
+        return "2xl:text-base";
+      case 18:
+        return "2xl:text-lg";
+      case 20:
+        return "2xl:text-xl";
+      case 24:
+        return "2xl:text-2xl";
+      case 28:
+        return "2xl:text-2.5xl";
+      case 30:
+        return "2xl:text-3xl";
+      case 36:
+        return "2xl:text-4xl";
+      case 48:
+        return "2xl:text-5xl";
+      case 60:
+        return "2xl:text-6xl";
+      case 72:
+        return "2xl:text-7xl";
+      case 96:
+        return "2xl:text-8xl";
+      case 128:
+        return "2xl:text-9xl";
+      default:
+        return "";
+    }
   };
 
   const getSize = (size: fontSize) => {
@@ -74,18 +246,17 @@ const Heading = ({
     }
   };
 
-  const extraLarge = xl && getSize(xl);
-  const large = lg && getPrefix(lg, "lg:");
-  const medium = md && getPrefix(md, "md:");
-  const small = sm && getPrefix(sm, "sm:");
+  const sizes = `${getSmallSize(sm)} ${getMediumSize(md)} ${getLargeSize(
+    lg,
+  )} ${getXLSize(xl)} ${get2XLSize(xxl)}`;
 
   if (heading === "h1") {
     const defaultSize = 28;
     return (
       <h1
-        className={`${
-          extraLarge ? extraLarge : getSize(defaultSize)
-        } ${small} ${medium} ${large} font-bold text-fog-of-war mb-4 ${className}`}
+        className={`${getSize(
+          size ? size : defaultSize,
+        )} ${sizes} font-bold text-fog-of-war mb-4 ${className}`}
       >
         {children}
       </h1>
@@ -93,14 +264,11 @@ const Heading = ({
   }
   if (heading === "h2") {
     const defaultSize = 28;
-    console.log(small);
     return (
       <h2
-        className={`${extraLarge ? extraLarge : getSize(defaultSize)} ${
-          sm && getPrefix(sm, "sm:")
-        } ${md && getPrefix(md, "md:")} ${
-          lg && getPrefix(lg, "lg:")
-        } font-bold text-fog-of-war mb-4 ${className}`}
+        className={`${getSize(
+          size ? size : defaultSize,
+        )} ${sizes} font-bold text-fog-of-war mb-4 ${className}`}
       >
         {children}
       </h2>
@@ -110,9 +278,9 @@ const Heading = ({
     const defaultSize = 18;
     return (
       <h3
-        className={`${
-          extraLarge ? extraLarge : getSize(defaultSize)
-        } ${large} ${medium} ${small} font-semibold text-fog-of-war mb-4 ${className}`}
+        className={`${getSize(
+          size ? size : defaultSize,
+        )} ${sizes} font-semibold text-fog-of-war mb-4 ${className}`}
       >
         {children}
       </h3>
@@ -121,11 +289,7 @@ const Heading = ({
   if (heading === "h4") {
     const defaultSize = 16;
     return (
-      <h4
-        className={`${
-          extraLarge ? extraLarge : getSize(defaultSize)
-        } ${large} ${medium} ${small}  font-semibold text-fog-of-war ${className}`}
-      >
+      <h4 className={`vfont-semibold text-fog-of-war ${className}`}>
         {children}
       </h4>
     );
@@ -135,9 +299,9 @@ const Heading = ({
 
     return (
       <h5
-        className={`${
-          extraLarge ? extraLarge : getSize(defaultSize)
-        } ${large} ${medium} ${small}  font-semibold text-fog-of-war ${className}`}
+        className={`${getSize(
+          size ? size : defaultSize,
+        )} ${sizes} font-semibold text-fog-of-war ${className}`}
       >
         {children}
       </h5>
@@ -147,9 +311,9 @@ const Heading = ({
     const defaultSize = 14;
     return (
       <h6
-        className={`${
-          extraLarge ? extraLarge : getSize(defaultSize)
-        } ${large} ${medium} ${small}  font-bold text-fog-of-war ${className}`}
+        className={`${getSize(
+          size ? size : defaultSize,
+        )} ${sizes} font-bold text-fog-of-war ${className}`}
       >
         {children}
       </h6>
