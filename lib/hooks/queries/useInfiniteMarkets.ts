73ef4badfc642d88e5ee663f074bd59fdd6fa064
robhyrk <rob.hyrkiel@gmail.com>
2023-03-23 13:16:36 -0400

    fix: hide markets with no liquidity when user selects liquidity only

--- lib/hooks/queries/useInfiniteMarkets.ts
+++ lib/hooks/queries/useInfiniteMarkets.ts
@@ -86,12 +86,13 @@ export const useInfiniteMarkets = (
         status_in: statuses.length === 0 ? undefined : statuses,
         tags_containsAny: tags.length === 0 ? undefined : tags,
         pool_isNull: withLiquidityOnly ? false : undefined,
-        pool:
-          currencies.length === 0
-            ? undefined
-            : {
-                baseAsset_in: currencies,
-              },
+        pool: withLiquidityOnly
+          ? { ztgQty_gt: 0 }
+          : undefined && currencies.length === 0
+          ? undefined
+          : {
+              baseAsset_in: currencies,
+            },
       },
       offset: !pageParam ? 0 : limit * pageParam,
       limit: limit,
