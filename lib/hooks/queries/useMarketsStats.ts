b7d22852290d6aec3a56eb0f922f332a37378cec
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-03-31 19:37:06 +0200

    dont fetch liqudity and participants for markets without liquidity pool

--- lib/hooks/queries/useMarketsStats.ts
+++ lib/hooks/queries/useMarketsStats.ts
@@ -2,6 +2,7 @@ import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { getMarketsStats, MarketStats } from "lib/gql/markets-stats";
 import { useStore } from "lib/stores/Store";
 import { useSdkv2 } from "../useSdkv2";
+import { useMarketsByIds } from "./useMarketsByIds";
 
 export const marketsStatsRootQuery = "marketsStats";
 
@@ -10,17 +11,49 @@ export const useMarketsStats = (
 ): UseQueryResult<MarketStats[]> => {
   const { graphQLClient } = useStore();
   const [sdk, id] = useSdkv2();
+  const { data: markets } = useMarketsByIds(
+    marketIds.map((id) => ({ marketId: id })),
+  );
+
+  const marketIdPoolIdMap =
+    markets?.map((market) => {
+      return {
+        marketId: market.marketId,
+        poolId: market.pool?.poolId,
+      };
+    }) ?? [];
 
   return useQuery<MarketStats[]>(
-    [marketsStatsRootQuery, id, marketIds],
-    () => {
+    [marketsStatsRootQuery, id, marketIds, marketIdPoolIdMap],
+    async () => {
       if (graphQLClient == null) {
         return [];
       }
-      return getMarketsStats(graphQLClient, marketIds);
+      const noPoolMarketIds = marketIdPoolIdMap
+        .filter((item) => item.poolId == null)
+        .map((item) => item.marketId);
+
+      const yesPoolmarketIds = marketIdPoolIdMap
+        .filter((item) => item.poolId != null)
+        .map((item) => item.marketId);
+
+      const yesPoolStats = await getMarketsStats(
+        graphQLClient,
+        yesPoolmarketIds,
+      );
+
+      const noPoolStats: MarketStats[] = noPoolMarketIds.map((id) => {
+        return { marketId: id, liquidity: "0", participants: 0 };
+      });
+
+      return [...yesPoolStats, ...noPoolStats];
     },
     {
-      enabled: graphQLClient != null || !!marketIds.length || sdk != null,
+      enabled:
+        graphQLClient != null ||
+        sdk != null ||
+        markets != null ||
+        marketIds.length > 0,
     },
   );
 };
