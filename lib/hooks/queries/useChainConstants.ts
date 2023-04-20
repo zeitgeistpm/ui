833730380dd61902b5563926bdf99525af2f7169
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-04-20 10:16:59 +0200

    keep data for the whole user session

--- lib/hooks/queries/useChainConstants.ts
+++ lib/hooks/queries/useChainConstants.ts
@@ -37,50 +37,60 @@ export type ChainConstants = {
 export const useChainConstants = () => {
   const [sdk, id] = useSdkv2();
 
-  return useQuery<ChainConstants>([id, sdk, "chain-constants"], async () => {
-    if (!isRpcSdk(sdk)) return null;
+  return useQuery<ChainConstants>(
+    [id, sdk, "chain-constants"],
+    async () => {
+      if (!isRpcSdk(sdk)) return null;
 
-    const [consts, properties] = await Promise.all([
-      sdk.api.consts,
-      sdk.api.rpc.system.properties(),
-    ]);
+      const [consts, properties] = await Promise.all([
+        sdk.api.consts,
+        sdk.api.rpc.system.properties(),
+      ]);
 
-    // minimumPeriod * 2 is fair assumption for now but need to make sure this stays up
-    // to date with the chain code
-    const blockTimeSec = (consts.timestamp.minimumPeriod.toNumber() * 2) / 1000;
-    const config: ChainConstants = {
-      tokenSymbol: properties.tokenSymbol
-        .toString()
-        .replace("[", "")
-        .replace("]", ""),
-      ss58Prefix: consts.system.ss58Prefix.toNumber(),
-      blockTimeSec: blockTimeSec,
-      markets: {
-        maxDisputes: consts.predictionMarkets.maxDisputes.toNumber(),
-        disputeBond: consts.predictionMarkets.disputeBond.toNumber() / ZTG,
-        disputeFactor: consts.predictionMarkets.disputeFactor.toNumber() / ZTG,
-        oracleBond: consts.predictionMarkets.oracleBond.toNumber() / ZTG,
-        advisoryBond: consts.predictionMarkets.advisoryBond.toNumber() / ZTG,
-        validityBond: consts.predictionMarkets.validityBond.toNumber() / ZTG,
-        maxCategories: consts.predictionMarkets.maxCategories.toNumber(),
-        minCategories: consts.predictionMarkets.minCategories.toNumber(),
-      },
-      court: {
-        caseDurationSec:
-          consts.court.courtCaseDuration.toNumber() * blockTimeSec,
-        stakeWeight: consts.court.stakeWeight.toNumber() / ZTG,
-      },
-      swaps: {
-        minLiquidity: consts.swaps.minLiquidity.toNumber() / ZTG,
-        exitFee: consts.swaps.exitFee.toNumber() / ZTG,
-      },
-      identity: {
-        basicDeposit: consts.identity.basicDeposit.toNumber() / ZTG,
-        fieldDeposit: consts.identity.fieldDeposit.toNumber() / ZTG,
-      },
-      balances: {
-        existentialDeposit: consts.balances.existentialDeposit.toNumber() / ZTG,
-      },
-    };
-  });
+      // minimumPeriod * 2 is fair assumption for now but need to make sure this stays up
+      // to date with the chain code
+      const blockTimeSec =
+        (consts.timestamp.minimumPeriod.toNumber() * 2) / 1000;
+      const config: ChainConstants = {
+        tokenSymbol: properties.tokenSymbol
+          .toString()
+          .replace("[", "")
+          .replace("]", ""),
+        ss58Prefix: consts.system.ss58Prefix.toNumber(),
+        blockTimeSec: blockTimeSec,
+        markets: {
+          maxDisputes: consts.predictionMarkets.maxDisputes.toNumber(),
+          disputeBond: consts.predictionMarkets.disputeBond.toNumber() / ZTG,
+          disputeFactor:
+            consts.predictionMarkets.disputeFactor.toNumber() / ZTG,
+          oracleBond: consts.predictionMarkets.oracleBond.toNumber() / ZTG,
+          advisoryBond: consts.predictionMarkets.advisoryBond.toNumber() / ZTG,
+          validityBond: consts.predictionMarkets.validityBond.toNumber() / ZTG,
+          maxCategories: consts.predictionMarkets.maxCategories.toNumber(),
+          minCategories: consts.predictionMarkets.minCategories.toNumber(),
+        },
+        court: {
+          caseDurationSec:
+            consts.court.courtCaseDuration.toNumber() * blockTimeSec,
+          stakeWeight: consts.court.stakeWeight.toNumber() / ZTG,
+        },
+        swaps: {
+          minLiquidity: consts.swaps.minLiquidity.toNumber() / ZTG,
+          exitFee: consts.swaps.exitFee.toNumber() / ZTG,
+        },
+        identity: {
+          basicDeposit: consts.identity.basicDeposit.toNumber() / ZTG,
+          fieldDeposit: consts.identity.fieldDeposit.toNumber() / ZTG,
+        },
+        balances: {
+          existentialDeposit:
+            consts.balances.existentialDeposit.toNumber() / ZTG,
+        },
+      };
+    },
+    {
+      keepPreviousData: true,
+      staleTime: Infinity,
+    },
+  );
 };
