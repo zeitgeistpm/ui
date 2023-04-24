eb7e4e11289d0e5f5dcf528a951813dd48465c79
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-04-24 13:39:43 +0300

    remove saturatedPoolIndex from PoolTable

--- components/liquidity/PoolTable.tsx
+++ components/liquidity/PoolTable.tsx
@@ -2,7 +2,9 @@ import { IOBaseAssetId, parseAssetId, ZTG } from "@zeitgeistpm/sdk-next";
 import Table, { TableColumn, TableData } from "components/ui/Table";
 import Decimal from "decimal.js";
 import { useAccountPoolAssetBalances } from "lib/hooks/queries/useAccountPoolAssetBalances";
+import { useAssetMetadata } from "lib/hooks/queries/useAssetMetadata";
 import { useAssetUsdPrice } from "lib/hooks/queries/useAssetUsdPrice";
+import { useMarket } from "lib/hooks/queries/useMarket";
 import { useMarketSpotPrices } from "lib/hooks/queries/useMarketSpotPrices";
 import { usePool } from "lib/hooks/queries/usePool";
 import { usePoolBaseBalance } from "lib/hooks/queries/usePoolBaseBalance";
@@ -41,45 +43,50 @@ const PoolTable = ({
   marketId: number;
 }) => {
   const { data: pool } = usePool({ poolId });
-  const { data: saturatedPoolIndex } = useSaturatedPoolsIndex(
-    pool ? [pool] : undefined,
-  );
-  const saturatedPoolData = saturatedPoolIndex?.[poolId];
+  const { data: market } = useMarket({ marketId });
+  const baseAssetId = parseAssetId(pool?.baseAsset).unrightOr(null);
+  const { data: metadata } = useAssetMetadata(baseAssetId);
+
   const { data: balances } = useAccountPoolAssetBalances(pool?.accountId, pool);
   const { data: basePoolBalance } = usePoolBaseBalance(poolId);
-  const { data: baseAssetUsdPrice } = useAssetUsdPrice(
-    parseAssetId(pool?.baseAsset).unrightOr(null),
-  );
+  const { data: baseAssetUsdPrice } = useAssetUsdPrice(baseAssetId);
   const { data: spotPrices } = useMarketSpotPrices(marketId);
 
-  const tableData: TableData[] = saturatedPoolData?.assets?.map(
-    (asset, index) => {
-      let amount: Decimal;
-      let usdValue: Decimal;
-      if (IOBaseAssetId.is(asset.assetId)) {
-        amount = basePoolBalance;
-        usdValue = basePoolBalance?.mul(baseAssetUsdPrice ?? 0);
-      } else {
-        amount = new Decimal(balances[index]?.free.toString() ?? asset.amount);
-        usdValue = amount
-          .mul(spotPrices?.get(index) ?? 0)
-          ?.mul(baseAssetUsdPrice ?? 0);
-      }
+  const tableData: TableData[] = pool?.weights?.map((asset, index) => {
+    let amount: Decimal;
+    let usdValue: Decimal;
+    let category;
+    const assetId = parseAssetId(asset.assetId).unrightOr(null);
 
-      return {
-        token: {
-          color: asset.category.color || "#ffffff",
-          label: asset.category.ticker,
-        },
-        weights: asset.percentage,
-        poolBalance: {
-          value: amount?.div(ZTG).toDecimalPlaces(2).toNumber(),
-          usdValue: usdValue?.div(ZTG).toDecimalPlaces(2).toNumber(),
-        },
-        manage: <ManageLiquidityButton poolId={poolId} />,
-      };
-    },
-  );
+    if (IOBaseAssetId.is(assetId)) {
+      amount = basePoolBalance;
+      usdValue = basePoolBalance?.mul(baseAssetUsdPrice ?? 0);
+      category = { color: "#ffffff", ticker: metadata?.symbol };
+    } else {
+      amount = new Decimal(balances[index]?.free.toString() ?? 0);
+      usdValue = amount
+        .mul(spotPrices?.get(index) ?? 0)
+        ?.mul(baseAssetUsdPrice ?? 0);
+      category = market?.categories[index];
+    }
+    console.log(category);
+
+    return {
+      token: {
+        color: category?.color || "#ffffff",
+        label: category?.ticker,
+      },
+      weights: new Decimal(asset.weight)
+        .div(pool.totalWeight)
+        .mul(100)
+        .toNumber(),
+      poolBalance: {
+        value: amount?.div(ZTG).toDecimalPlaces(2).toNumber(),
+        usdValue: usdValue?.div(ZTG).toDecimalPlaces(2).toNumber(),
+      },
+      manage: <ManageLiquidityButton poolId={poolId} />,
+    };
+  });
 
   return <Table data={tableData} columns={columns} />;
 };
