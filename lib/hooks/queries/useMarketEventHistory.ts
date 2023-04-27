298508d054ec265d816575840597c1a313d395e1
robhyrk <rob.hyrkiel@gmail.com>
2023-04-27 18:19:56 -0400

    add comments

--- lib/hooks/queries/useMarketEventHistory.ts
+++ lib/hooks/queries/useMarketEventHistory.ts
@@ -7,6 +7,11 @@ import { useTimeStampForBlock } from "./useTimeStampForBlock";
 
 export const marketsEventsRootQuery = "marketsEvents";
 
+//is a market always reported first before any disputes can be made?
+//if the oracle doesnt report the result then who is the reporter? (i.e. did the oracle fail to report?)
+//who is the "Authority"? is that the "Authorized Address"? Does the Authority ultimatrely decide the outcome of the market?
+//are all markets resolved? or can they simply end?
+
 export const useMarketEventHistory = (
   marketId: string,
 ): UseQueryResult<MarketPageIndexedData> => {
@@ -22,6 +27,7 @@ export const useMarketEventHistory = (
       const disputes = market.disputes;
       const report = market.report;
 
+      //if oracle report is true, then oracle reported otherwise market reporter reported
       const oracleReported = report.by === market.oracle;
       console.log(oracleReported);
       // const { data: timeStamp } = useTimeStampForBlock(1838978);
