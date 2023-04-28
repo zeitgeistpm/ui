a1cd17757260c58a7a141f5a42c94052111fe362
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-04-28 15:03:47 +0200

    multistep dialog for filters with initial transition

--- components/markets/market-filter/MobileDialog.tsx
+++ components/markets/market-filter/MobileDialog.tsx
@@ -1,24 +1,84 @@
-import { Dialog } from "@headlessui/react";
-import { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { useEffect, useState } from "react";
 
 export type MobileDialogProps = {
   open: boolean;
   setOpen: (open: boolean) => void;
 };
 
+const AllFilters = ({ setNext }) => {
+  return (
+    <>
+      <p>Main Fitlers screen</p>
+      <a className="block cursor-pointer" onClick={() => setNext()}>
+        Next
+      </a>
+    </>
+  );
+};
+
+const FilterSelection = ({ setPrev }) => {
+  return (
+    <>
+      <p>Filter Selection</p>
+      <a className="block cursor-pointer" onClick={() => setPrev()}>
+        Prev
+      </a>
+    </>
+  );
+};
+
 const MobileDialog = ({ open, setOpen }: MobileDialogProps) => {
+  const [showTransition, setShowTransition] = useState(open);
+  const [step, setStep] = useState(0);
+
+  const setNext = () => {
+    setStep((step) => step + 1);
+  };
+
+  const setPrev = () => {
+    setStep((step) => step - 1);
+  };
+
+  useEffect(() => {
+    if (open == null) {
+      return;
+    }
+    setShowTransition(open);
+  }, [open]);
+
   return (
     <Dialog
       open={open}
-      onClose={() => setOpen(false)}
-      className="relative z-40"
+      onClose={() => {
+        setTimeout(() => {
+          setOpen(false);
+        }, 100);
+        setShowTransition(false);
+      }}
+      className="fixed top-0 left-0 w-full h-full bg-transparent z-50"
     >
-      <div className="fixed w-full h-full bg-white top-0">
+      <Transition
+        show={showTransition}
+        enter="transition duration-100 ease-out"
+        enterFrom="transform -translate-y-full"
+        enterTo="transform translate-y-0"
+        leave="transition duration-100 ease-out"
+        leaveFrom="transform translate-y-0"
+        leaveTo="transform -translate-y-full"
+        className="absolute w-full h-full top-0 left-0 bg-white"
+        appear={true}
+      >
         <Dialog.Panel>
           <Dialog.Title>Filters</Dialog.Title>
-          <p>I am mobile content</p>
+          {
+            {
+              0: <AllFilters setNext={setNext} />,
+              1: <FilterSelection setPrev={setPrev} />,
+            }[step]
+          }
         </Dialog.Panel>
-      </div>
+      </Transition>
     </Dialog>
   );
 };
