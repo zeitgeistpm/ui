40d3d2193f3f79bb8a6ed9074e5fec5262aa40bc
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-04-28 11:22:53 +0200

    smoother text blur in line with image transition duration

--- components/hero-slider/HeroSlider.tsx
+++ components/hero-slider/HeroSlider.tsx
@@ -51,12 +51,12 @@ const HeroSlider = ({
           <Transition
             className={"absolute h-full w-full"}
             show={index == slider.currentSlide}
-            enter="transition-all duration-1000"
-            enterFrom="opacity-0 blur-md"
+            enter="transition-all duration-500"
+            enterFrom="opacity-0 blur"
             enterTo="opacity-100 blur-none"
             leave="transition-all duration-1000"
             leaveFrom="opacity-100 blur-none"
-            leaveTo="opacity-0 blur-md"
+            leaveTo="opacity-0 blur"
           >
             <HeroSlide className="absolute h-full w-full" banner={banner} />
           </Transition>
