76083ae228973db5bf48e2e40651a5a1d2b25759
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-05-18 19:26:51 +0200

    fix step type

--- lib/state/market-creation/types/step.ts
+++ lib/state/market-creation/types/step.ts
@@ -13,12 +13,12 @@ export type MarketCreationStepType =
 export type MarketCreationStep = WizardStepData<MarketCreationStepType>;
 
 export const marketCreationSteps: MarketCreationStep[] = [
-  { label: "Currency", isValid: false },
-  { label: "Question", isValid: false },
-  { label: "Answers", isValid: false },
-  { label: "Time Period", isValid: false },
-  { label: "Oracle", isValid: false },
-  { label: "Description", isValid: false },
-  { label: "Moderation", isValid: false },
-  { label: "Preview", isValid: false },
+  { label: "Currency", isValid: false, isTouched: false },
+  { label: "Question", isValid: false, isTouched: false },
+  { label: "Answers", isValid: false, isTouched: false },
+  { label: "Time Period", isValid: false, isTouched: false },
+  { label: "Oracle", isValid: false, isTouched: false },
+  { label: "Description", isValid: false, isTouched: false },
+  { label: "Moderation", isValid: false, isTouched: false },
+  { label: "Preview", isValid: false, isTouched: false },
 ];
