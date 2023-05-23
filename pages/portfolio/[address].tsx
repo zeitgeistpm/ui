f6062115c770b0f60420a213a1d45e0390eb7255
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-05-23 12:46:42 +0300

    rework current portfolio content

--- pages/portfolio/[address].tsx
+++ pages/portfolio/[address].tsx
@@ -3,13 +3,13 @@ import { getIndexOf } from "@zeitgeistpm/sdk-next";
 import BondsTable from "components/portfolio/BondsTable";
 import { PortfolioBreakdown } from "components/portfolio/Breakdown";
 import EmptyPortfolio from "components/portfolio/EmptyPortfolio";
+import HistoryTabGroup from "components/portfolio/HistoryTabGroup";
 import {
   MarketPositions,
   MarketPositionsSkeleton,
 } from "components/portfolio/MarketPositions";
-import TradeHistoryTable from "components/portfolio/TradeHistoryTable";
-import TransactionHistoryTable from "components/portfolio/TransactionHistoryTable";
 import InfoBoxes from "components/ui/InfoBoxes";
+import SubTabsList from "components/ui/SubTabsList";
 import { usePortfolioPositions } from "lib/hooks/queries/usePortfolioPositions";
 import { useZtgPrice } from "lib/hooks/queries/useZtgPrice";
 import { groupBy, range } from "lodash-es";
@@ -41,98 +41,94 @@ const Portfolio: NextPage = () => {
   return (
     <>
       <InfoBoxes />
-      <Tab.Group>
-        <Tab.List className="flex center my-14">
-          <Tab className="px-4 ui-selected:font-bold ui-selected:text-black text-sky-600 transition-all">
-            Portfolio
-          </Tab>
-          <Tab className="px-4 ui-selected:font-bold ui-selected:text-black text-sky-600 transition-all">
-            Activity
-          </Tab>
-        </Tab.List>
-        <Tab.Panels>
-          <Tab.Panel>
-            {" "}
-            <div className="mb-12">
-              <h2 className="text-2xl mb-6 text-center">Breakdown</h2>
-              <PortfolioBreakdown
-                {...(breakdown ?? {
-                  loading: true,
-                })}
-              />
-            </div>
-            <div className="mb-12">
-              <h2 className="text-2xl mb-6 text-center">Predictions</h2>
-              <Tab.Group>
-                <Tab.List className="flex center mb-14">
-                  {["By Markets", "Subsidy", "Bonds"].map((title, index) => (
-                    <Tab className="px-4" key={index}>
-                      {({ selected }) => (
-                        <div
-                          className={
-                            selected
-                              ? "font-semibold text-black transition-all"
-                              : "text-sky-600 transition-all"
-                          }
-                        >
-                          {title}
-                        </div>
-                      )}
-                    </Tab>
-                  ))}
-                </Tab.List>
+      <div className="mb-12">
+        <h2 className="text-2xl mb-6 text-center">Summary</h2>
+        <PortfolioBreakdown
+          {...(breakdown ?? {
+            loading: true,
+          })}
+        />
+      </div>
+      <div className="mb-12">
+        <Tab.Group>
+          <Tab.List className="flex center mb-4">
+            {["Predictions", "Balances", "Markets", "Badges", "History"].map(
+              (title, index) => (
+                <Tab className="px-4" key={index}>
+                  {({ selected }) => (
+                    <div
+                      className={
+                        selected
+                          ? "font-semibold text-black transition-all"
+                          : "text-sky-600 transition-all"
+                      }
+                    >
+                      {title}
+                    </div>
+                  )}
+                </Tab>
+              ),
+            )}
+          </Tab.List>
 
+          <Tab.Panels>
+            <Tab.Panel>
+              {!marketPositionsByMarket || !ztgPrice ? (
+                range(0, 8).map((i) => (
+                  <MarketPositionsSkeleton className="mb-14" key={i} />
+                ))
+              ) : Object.values(marketPositionsByMarket).length > 0 ? (
+                Object.values(marketPositionsByMarket).map(
+                  (marketPositions) => {
+                    const market = marketPositions[0].market;
+
+                    marketPositions = marketPositions.filter((position) =>
+                      position.userBalance.gt(0),
+                    );
+
+                    if (
+                      market.status === "Resolved" &&
+                      market.marketType.categorical
+                    ) {
+                      marketPositions = marketPositions.filter(
+                        (position) =>
+                          getIndexOf(position.assetId) ===
+                          Number(market.resolvedOutcome),
+                      );
+                    }
+
+                    if (marketPositions.length === 0) return null;
+
+                    return (
+                      <MarketPositions
+                        key={market.marketId}
+                        className="mb-14 border-b-4 border-gray-200"
+                        market={market}
+                        usdZtgPrice={ztgPrice}
+                        positions={marketPositions.filter((position) =>
+                          position.userBalance.gt(0),
+                        )}
+                      />
+                    );
+                  },
+                )
+              ) : (
+                <EmptyPortfolio
+                  headerText="You don't have any assets"
+                  bodyText="View markets to trade assets"
+                  buttonText="View Markets"
+                  buttonLink="/markets"
+                />
+              )}
+            </Tab.Panel>
+            <Tab.Panel>Add currencies table here</Tab.Panel>
+            <Tab.Panel>
+              <Tab.Group>
+                <SubTabsList titles={["Created Markets", "Liquidity"]} />
                 <Tab.Panels>
                   <Tab.Panel>
-                    {!marketPositionsByMarket || !ztgPrice ? (
-                      range(0, 8).map((i) => (
-                        <MarketPositionsSkeleton className="mb-14" key={i} />
-                      ))
-                    ) : Object.values(marketPositionsByMarket).length > 0 ? (
-                      Object.values(marketPositionsByMarket).map(
-                        (marketPositions) => {
-                          const market = marketPositions[0].market;
-
-                          marketPositions = marketPositions.filter((position) =>
-                            position.userBalance.gt(0),
-                          );
-
-                          if (
-                            market.status === "Resolved" &&
-                            market.marketType.categorical
-                          ) {
-                            marketPositions = marketPositions.filter(
-                              (position) =>
-                                getIndexOf(position.assetId) ===
-                                Number(market.resolvedOutcome),
-                            );
-                          }
-
-                          if (marketPositions.length === 0) return null;
-
-                          return (
-                            <MarketPositions
-                              key={market.marketId}
-                              className="mb-14 border-b-4 border-gray-200"
-                              market={market}
-                              usdZtgPrice={ztgPrice}
-                              positions={marketPositions.filter((position) =>
-                                position.userBalance.gt(0),
-                              )}
-                            />
-                          );
-                        },
-                      )
-                    ) : (
-                      <EmptyPortfolio
-                        headerText="You don't have any assets"
-                        bodyText="View markets to trade assets"
-                        buttonText="View Markets"
-                        buttonLink="/markets"
-                      />
-                    )}
+                    <BondsTable address={address} />
                   </Tab.Panel>
-
                   <Tab.Panel>
                     {!subsidyPositionsByMarket || !ztgPrice ? (
                       range(0, 8).map((i) => (
@@ -164,44 +160,16 @@ const Portfolio: NextPage = () => {
                       />
                     )}
                   </Tab.Panel>
-                  <Tab.Panel>
-                    <BondsTable address={address} />
-                  </Tab.Panel>
                 </Tab.Panels>
               </Tab.Group>
-            </div>
-          </Tab.Panel>
-          <Tab.Panel>
-            <Tab.Group>
-              <Tab.List className="flex center mb-14">
-                {["Trades", "Other Transactions"].map((title, index) => (
-                  <Tab className="px-4" key={index}>
-                    {({ selected }) => (
-                      <div
-                        className={
-                          selected
-                            ? "font-semibold text-black transition-all"
-                            : "text-sky-600 transition-all"
-                        }
-                      >
-                        {title}
-                      </div>
-                    )}
-                  </Tab>
-                ))}
-              </Tab.List>
-              <Tab.Panels>
-                <Tab.Panel>
-                  <TradeHistoryTable address={address} />
-                </Tab.Panel>
-                <Tab.Panel>
-                  <TransactionHistoryTable address={address} />
-                </Tab.Panel>
-              </Tab.Panels>
-            </Tab.Group>
-          </Tab.Panel>
-        </Tab.Panels>
-      </Tab.Group>
+            </Tab.Panel>
+            <Tab.Panel>badges</Tab.Panel>
+            <Tab.Panel>
+              <HistoryTabGroup address={address} />
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
     </>
   );
 };
