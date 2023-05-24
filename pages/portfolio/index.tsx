3c6ecfe9a128a9630548d979f7d09adeae92de4a
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-05-24 19:47:29 +0200

    remove imports

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,9 +1,5 @@
-import EmptyPortfolio from "components/portfolio/EmptyPortfolio";
 import PortfolioLayout from "layouts/PortfolioLayout";
 import { NextPageWithLayout } from "layouts/types";
-import { useWallet } from "lib/state/wallet";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 const PortfolioIndex: NextPageWithLayout = () => {
   return <></>;
