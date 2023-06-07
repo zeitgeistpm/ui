026fe90b080d97d4c4ee55d13f23d48d2f1a69cd
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-06-07 14:57:19 +0200

    use baseCurrency from market instead of pool schema

--- lib/hooks/queries/useInfiniteMarkets.ts
+++ lib/hooks/queries/useInfiniteMarkets.ts
@@ -65,9 +65,9 @@ export const useInfiniteMarkets = (
         status_in: statuses.length === 0 ? undefined : statuses,
         tags_containsAny: tags?.length === 0 ? undefined : tags,
         pool_isNull: withLiquidityOnly ? false : undefined,
+        baseAsset_in: currencies?.length !== 0 ? currencies : undefined,
         pool: {
           baseAssetQty_gt: withLiquidityOnly ? 0 : undefined,
-          baseAsset_in: currencies?.length !== 0 ? currencies : undefined,
         },
       },
       offset: !pageParam ? 0 : limit * pageParam,
