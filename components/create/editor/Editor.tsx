c1049fcb0f76f66173a78adadd71009ae6bf079c
Yornaath <jornandretangen@gmail.com>
2023-06-08 12:59:19 +0200

    Update components/create/editor/Editor.tsx
    Co-authored-by: Tom Robiquet <tom-robiquet@hotmail.co.uk>

--- components/create/editor/Editor.tsx
+++ components/create/editor/Editor.tsx
@@ -244,7 +244,7 @@ export const MarketEditor = () => {
                 </p>
                 <h4 className="text-base text-left mb-1">Scalar</h4>
                 <p className="text-gray-500 font-light text-sm text-left mb-4">
-                  A scalar market is a market where the outcome is a number in a
+                  A scalar market is a market where the outcome is a number or date in a
                   the range specified by the lower(<b>short</b>) and upper(
                   <b>long</b>) bound.{" "}
                   <a
