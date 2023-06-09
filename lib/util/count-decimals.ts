be2c5f4cf74341048e925909613d17b01ee21e95
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-06-09 16:52:44 +0300

    handle very small numbers

--- lib/util/count-decimals.ts
+++ lib/util/count-decimals.ts
@@ -1,4 +1,9 @@
 export const countDecimals = (val: number) => {
   if (Math.floor(val.valueOf()) === val.valueOf()) return 0;
-  return val.toString().split(".")[1].length || 0;
+  // handle very small numbers
+  try {
+    return val.toString().split(".")[1].length || 0;
+  } catch {
+    return 0;
+  }
 };
