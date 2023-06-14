dfecf759680aa34150eef2fe2719221853915c96
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-06-14 22:39:21 +0200

    fix formatNumberCompact

--- lib/util/format-scalar-outcome.ts
+++ lib/util/format-scalar-outcome.ts
@@ -7,16 +7,16 @@ export const formatScalarOutcome = (
   outcome: string | number,
   scalarType: ScalarRangeType,
 ) => {
-  return scalarType === "number"
-    ? new Intl.NumberFormat("default", {
+  return scalarType === "date"
+    ? new Intl.DateTimeFormat("default", {
+        dateStyle: "medium",
+      }).format(new Decimal(outcome).div(ZTG).toNumber())
+    : new Intl.NumberFormat("default", {
         maximumSignificantDigits: 3,
         notation: "compact",
       }).format(
         typeof outcome === "string"
           ? new Decimal(outcome).div(ZTG).toNumber()
           : outcome,
-      )
-    : new Intl.DateTimeFormat("default", {
-        dateStyle: "medium",
-      }).format(new Decimal(outcome).div(ZTG).toNumber());
+      );
 };
