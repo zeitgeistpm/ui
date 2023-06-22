8fda40b479eca7f9d2d2a2f340bbdfe4198731de
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-06-22 19:32:23 +0300

    disable checks

--- components/portfolio/DepositButton.tsx
+++ components/portfolio/DepositButton.tsx
@@ -207,6 +207,7 @@ const DepositModal = ({
           <FormTransactionButton
             className="w-full max-w-[250px]"
             disabled={formState.isValid === false || isLoading}
+            disableFeeCheck={true}
           >
             Confirm Deposit
           </FormTransactionButton>
