6f4e0360ad20c06bc4b0832b0b2539d4b5e285b0
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-06-27 15:06:09 +0100

    remove commented code

--- lib/util/calc-free-balance.ts
+++ lib/util/calc-free-balance.ts
@@ -5,9 +5,6 @@ export const calculateFreeBalance = (
   miscFrozen: string,
   feeFrozen: string,
 ) => {
-  //   const maxFrozen = new Decimal(miscFrozen).greaterThan(feeFrozen)
-  //     ? new Decimal(miscFrozen)
-  //     : new Decimal(feeFrozen);
   const maxFrozen = Decimal.max(miscFrozen, feeFrozen);
   return new Decimal(free).minus(maxFrozen);
 };
