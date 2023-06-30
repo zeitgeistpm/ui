361ea61427a5694b14764723dae863ce036a9a87
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-06-30 11:29:07 +0100

    remove console logs

--- lib/util/await-indexer.ts
+++ lib/util/await-indexer.ts
@@ -2,8 +2,6 @@ const INDEXER_DELAY_MS = 40 * 1000;
 
 export const awaitIndexer = async (callback: () => void) => {
   setTimeout(() => {
-    console.log("called");
-
     callback();
   }, INDEXER_DELAY_MS);
 };
