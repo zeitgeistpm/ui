2f4fe5f56e14bf5cf2c0b984e27f264a53775752
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-07-04 14:07:55 +0200

    Revert "trigger validation whenever setValue runs"
    This reverts commit 312154c10687585a33a4943073fade7c4dcec9f8.

--- components/portfolio/DepositButton.tsx
+++ components/portfolio/DepositButton.tsx
@@ -134,6 +134,7 @@ const DepositModal = ({
           "amount",
           maxTransferAmount.mul(value.percentage).div(100).div(ZTG).toNumber(),
         );
+        trigger("amount");
       } else if (name === "amount" && value.amount !== "") {
         setValue(
           "percentage",
@@ -144,7 +145,6 @@ const DepositModal = ({
             .toString(),
         );
       }
-      trigger("amount");
     });
     return () => subscription.unsubscribe();
   }, [watch, balance, fee]);
