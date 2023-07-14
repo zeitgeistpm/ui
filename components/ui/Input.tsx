233a93fe87e0f240f93241264d348722e3da8b7d
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2023-07-14 15:12:42 +0200

    add input component with unwanted events disabled

---
+++ components/ui/Input.tsx
@@ -0,0 +1,96 @@
+import React, { FocusEventHandler, useEffect } from "react";
+
+type InputProps = {} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const defaultClassName =
+  "border-1 rounded-md border-transparent bg-anti-flash-white outline-none px-3 items-center";
+
+const Input: React.FC<InputProps> = React.forwardRef<
+  HTMLInputElement,
+  InputProps
+>((props, ref) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [focused, setFocused] = React.useState(false);
+
+  let { className, ...restProps } = props;
+  className = `${defaultClassName} ${className ?? ""}`;
+  const isNumber = props.type === "number";
+
+  if (isNumber) {
+    className += " font-mono";
+  }
+
+  useEffect(() => {
+    if (!isNumber || !inputRef.current) {
+      return;
+    }
+
+    const handleFocus = (e: FocusEvent) => {
+      setFocused(true);
+    };
+
+    const handleBlur = (e: FocusEvent) => {
+      setFocused(false);
+    };
+
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (
+        e.key === "e" ||
+        e.key === "E" ||
+        e.key === "Up" ||
+        e.key === "Down"
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (["ArrowUp", "ArrowDown"].includes(e.key)) {
+        e.preventDefault();
+      }
+    };
+
+    inputRef.current.addEventListener("focus", handleFocus);
+    inputRef.current.addEventListener("blur", handleBlur);
+    inputRef.current.addEventListener("keypress", handleKeyPress);
+    inputRef.current.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      inputRef.current?.removeEventListener("focus", handleFocus);
+      inputRef.current?.removeEventListener("blur", handleBlur);
+      inputRef.current?.removeEventListener("keypress", handleKeyPress);
+      inputRef.current?.removeEventListener("keydown", onKeyDown);
+    };
+  }, [isNumber, inputRef.current]);
+
+  useEffect(() => {
+    if (!(isNumber && inputRef.current && focused)) {
+      return;
+    }
+
+    const disableWheel = (e: WheelEvent) => {
+      e.preventDefault();
+    };
+
+    inputRef.current.addEventListener("wheel", disableWheel);
+
+    return () => inputRef.current?.removeEventListener("wheel", disableWheel);
+  }, [isNumber, inputRef.current, focused]);
+
+  return (
+    <input
+      {...restProps}
+      className={className}
+      ref={(instance) => {
+        inputRef.current = instance;
+        if (typeof ref === "function") {
+          ref(instance);
+        } else if (ref) {
+          ref.current = instance;
+        }
+      }}
+    />
+  );
+});
+
+export default Input;
