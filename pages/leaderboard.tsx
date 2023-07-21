1a5ed9977fb6961d53e419ef4ee95c31f4e69bf2
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-07-21 14:11:23 +0200

    merge

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,3 +1,5 @@
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { FullHistoricalAccountBalanceFragment } from "@zeitgeistpm/indexer";
 import {
   BaseAssetId,
   create,
@@ -8,6 +10,7 @@ import {
   parseAssetId,
   ZeitgeistIpfs,
 } from "@zeitgeistpm/sdk-next";
+import Avatar from "components/ui/Avatar";
 import Decimal from "decimal.js";
 import {
   endpointOptions,
@@ -15,18 +18,14 @@ import {
   graphQlEndpoint,
   ZTG,
 } from "lib/constants";
-import {
-  FOREIGN_ASSET_METADATA,
-  lookupAssetSymbol,
-} from "lib/constants/foreign-asset";
+import { FOREIGN_ASSET_METADATA } from "lib/constants/foreign-asset";
+import { getDisplayName } from "lib/gql/display-name";
+import { getAvatarParts } from "lib/gql/get-avatar";
+import { calcScalarResolvedPrices } from "lib/util/calc-scalar-winnings";
+import { createAvatarSdk } from "lib/util/create-avatar-sdk";
+import { parseAssetIdString } from "lib/util/parse-asset-id";
 import { NextPage } from "next";
 import Link from "next/link";
-import Avatar from "components/ui/Avatar";
-import { formatNumberCompact } from "lib/util/format-compact";
-import { parseAssetIdString } from "lib/util/parse-asset-id";
-import { FullHistoricalAccountBalanceFragment } from "@zeitgeistpm/indexer";
-import { calcScalarResolvedPrices } from "lib/util/calc-scalar-winnings";
-import { getDisplayName } from "lib/gql/display-name";
 
 // Approach: aggregate base asset movements in and out of a market
 // "In events": swaps, buy full set
@@ -77,6 +76,7 @@ type Rank = {
   accountId: string;
   profitUsd: number;
   name?: string;
+  avatarParts;
   markets: MarketSummary[];
 };
 
@@ -187,11 +187,14 @@ const getBaseAssetHistoricalPrices = async (): Promise<BasePrices> => {
 };
 
 export async function getStaticProps() {
-  const sdk = await create({
-    provider: endpointOptions.map((e) => e.value),
-    indexer: graphQlEndpoint,
-    storage: ZeitgeistIpfs(),
-  });
+  const [sdk, avatarSdk] = await Promise.all([
+    create({
+      provider: endpointOptions.map((e) => e.value),
+      indexer: graphQlEndpoint,
+      storage: ZeitgeistIpfs(),
+    }),
+    createAvatarSdk(),
+  ]);
 
   const basePrices = await getBaseAssetHistoricalPrices();
 
@@ -416,8 +419,22 @@ export async function getStaticProps() {
     top20.map((p) => p.accountId),
   );
 
+  const queryClient = new QueryClient();
+
+  // const avatarParts = await Promise.all(
+  //   top20.map((player) => getAvatarParts(avatarSdk, player.accountId)),
+  // );
+  const avatarParts = await Promise.all(
+    top20.map((player) =>
+      queryClient.prefetchQuery(["avatars", player.accountId], () =>
+        getAvatarParts(avatarSdk, player.accountId),
+      ),
+    ),
+  );
+
   return {
     props: {
+      dehydratedState: dehydrate(queryClient),
       rankings: top20.map((player, index) => ({
         ...player,
         name: names[index],
