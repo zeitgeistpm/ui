8b15a489352cb34646738d4e3daf177856b749ab
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-08-03 13:12:54 +0200

    Lazy load grill chat

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
+import { FC, PropsWithChildren, useRef, useState } from "react";
 import { useResizeDetector } from "react-resize-detector";
 import Image from "next/image";
 import dynamic from "next/dynamic";
@@ -29,7 +29,6 @@ const DefaultLayout: FC<PropsWithChildren> = ({ children }) => {
   const router = useRouter();
   useSubscribeBlockEvents();
   const [tradeItem, setTradeItem] = useState<TradeItem | null>(null);
-  const [showChat, setShowChat] = useState(false);
 
   const {
     width,
@@ -93,9 +92,7 @@ const DefaultLayout: FC<PropsWithChildren> = ({ children }) => {
       </TradeItemContext.Provider>
       <Account />
       <Onboarding />
-      {process.env.NEXT_PUBLIC_GRILLCHAT_DISABLE !== "true" && (
-        <GrillChat open={showChat} setOpen={setShowChat} />
-      )}
+      {process.env.NEXT_PUBLIC_GRILLCHAT_DISABLE !== "true" && <GrillChat />}
     </div>
   );
 };
