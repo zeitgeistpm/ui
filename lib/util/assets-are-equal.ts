4d0b86013314cf17fd7b211da38aa2ceabfc48e1
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-08-04 13:40:03 +0200

    show when portfolio is indexing

---
+++ lib/util/assets-are-equal.ts
@@ -0,0 +1,5 @@
+import { AssetId } from "@zeitgeistpm/sdk-next";
+
+export const assetsAreEqual = (asset1: AssetId, asset2: AssetId) => {
+  return JSON.stringify(asset1) === JSON.stringify(asset2);
+};
