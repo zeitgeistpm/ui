c06b8a789110ebee34f531f24f2913826a37fadd
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-08-04 15:47:08 +0200

    align left

--- components/portfolio/MarketPositionHeader.tsx
+++ components/portfolio/MarketPositionHeader.tsx
@@ -8,7 +8,7 @@ const MarketPositionHeader = ({
   question?: string;
 }) => {
   return (
-    <h3 className="text-ztg-16-150 text-center mb-6 font-normal">
+    <h3 className="text-ztg-16-150 mb-6 font-normal">
       <Link href={`/markets/${marketId}`}>{question}</Link>
     </h3>
   );
