16ab2ff8ff2793bfe12d28f901043f3a28142b49
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-08-07 13:16:22 +0200

    only show active pools in trending markets

--- lib/gql/trending-markets.ts
+++ lib/gql/trending-markets.ts
@@ -20,6 +20,7 @@ import { fetchZTGInfo } from "@zeitgeistpm/utility/dist/ztg";
 import { parseAssetIdString } from "lib/util/parse-asset-id";
 import { getDisplayName } from "./display-name";
 import { fetchAllPages } from "lib/util/fetch-all-pages";
+import { PoolStatus } from "@zeitgeistpm/indexer";
 
 const poolChangesQuery = gql`
   query PoolChanges($start: DateTime, $end: DateTime) {
@@ -111,6 +112,7 @@ const getTrendingMarkets = async (
     const { pools } = await sdk.indexer.pools({
       limit: limit,
       offset: pageNumber * limit,
+      where: { status_eq: PoolStatus.Active },
     });
     return pools;
   });
@@ -244,7 +246,7 @@ const calcTrendingPools = (
     dVolume: string;
   }[],
   basePrices: BasePrices,
-  pools: { poolId: number; baseAsset: string }[],
+  pools: { poolId: number; baseAsset: string; status: string }[],
 ) => {
   const poolVolumes: { [key: string]: Decimal } = {};
   const maxPools = 8;
