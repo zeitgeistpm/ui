4f340087286abe323b954b46869d57bfc9b0cfb4
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-08-15 13:00:03 +0200

    use fee paying asset

--- lib/hooks/queries/useFeePayingAsset.ts
+++ lib/hooks/queries/useFeePayingAsset.ts
@@ -1,27 +1,43 @@
-import { useQuery } from "@tanstack/react-query";
-import { ZTG } from "@zeitgeistpm/sdk-next";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { AssetId, ZTG, isRpcSdk } from "@zeitgeistpm/sdk-next";
 import { useWallet } from "lib/state/wallet";
 import { useSdkv2 } from "../useSdkv2";
 import { useExtrinsicFee } from "./useExtrinsicFee";
 import { useForeignAssetBalances } from "./useForiegnAssetBalances";
 import { useZtgBalance } from "./useZtgBalance";
+import { useChainConstants } from "./useChainConstants";
+import Decimal from "decimal.js";
 
 export const feePayingAssetRootKey = "fee-paying-asset";
 
-export const useFeePayingAsset = () => {
+type FeeAsset = {
+  assetId: AssetId;
+  symbol: string;
+  // feeEstimate: Decimal;
+};
+
+export const useFeePayingAsset = (): UseQueryResult<FeeAsset> => {
   const [sdk, id] = useSdkv2();
   const wallet = useWallet();
-  const extrinsic = wallet.activeAccount?.address
-    ? sdk
-        ?.asRpc()
-        .api.tx.balances.transfer(wallet.activeAccount?.address, ZTG.toFixed(0))
-    : undefined;
+  const extrinsic =
+    wallet.activeAccount?.address && isRpcSdk(sdk)
+      ? sdk.api.tx.balances.transfer(
+          wallet.activeAccount?.address,
+          ZTG.toFixed(0),
+        )
+      : undefined;
 
   const { data: fee } = useExtrinsicFee(extrinsic);
   const { data: nativeBalance } = useZtgBalance(wallet.activeAccount?.address);
   const { data: foreignAssetBalances } = useForeignAssetBalances(
     wallet.activeAccount?.address,
   );
+  const { data: constants } = useChainConstants();
+
+  const nativeAsset: FeeAsset = {
+    assetId: { Ztg: null },
+    symbol: constants?.tokenSymbol ?? "",
+  };
 
   const enabled = !!fee && !!nativeBalance && !!foreignAssetBalances;
 
@@ -30,7 +46,7 @@ export const useFeePayingAsset = () => {
     async () => {
       if (enabled) {
         if (nativeBalance.greaterThanOrEqualTo(fee)) {
-          return "ZTG";
+          return nativeAsset;
         }
 
         // find first available asset to pay fee, else just return native asset
@@ -40,9 +56,14 @@ export const useFeePayingAsset = () => {
         );
 
         if (availableAsset) {
-          return availableAsset.symbol;
+          return {
+            assetId: {
+              ForeignAsset: availableAsset.foreignAssetId,
+              symbol: availableAsset.symbol,
+            },
+          };
         } else {
-          return "ZTG";
+          return nativeAsset;
         }
       }
     },
@@ -51,5 +72,6 @@ export const useFeePayingAsset = () => {
     },
   );
 
+  //@ts-ignore
   return query;
 };
