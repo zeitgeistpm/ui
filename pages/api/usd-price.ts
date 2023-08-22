0932b92aee03b07f296579a8155b6944feddc0e2
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-08-22 13:46:51 +0200

    add data fetching and caching

--- pages/api/usd-price.ts
+++ pages/api/usd-price.ts
@@ -1,24 +1,33 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const runtime = "edge"; // 'nodejs' is the default
+export const config = {
+  runtime: "edge",
+};
 
-export default async function GET(request: NextRequest) {
-  const coinGeckoId = "zeitgeist";
+export default async function handler(request: NextRequest) {
+  const asset = request.nextUrl.searchParams.get("asset");
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/simple/price?ids=${coinGeckoId}&vs_currencies=usd`,
+    `https://api.coingecko.com/api/v3/simple/price?ids=${asset}&vs_currencies=usd`,
   );
 
   const json = await res.json();
 
+  const price = asset ? json[asset]?.usd : null;
+
   return NextResponse.json(
     {
-      body: request.body,
+      body: { price },
       query: request.nextUrl.search,
       cookies: request.cookies.getAll(),
     },
     {
       status: 200,
+      headers: {
+        "Vercel-CDN-Cache-Control": "public, s-maxage=300", //Vercel's Edge Cache to have a TTL (Time To Live) of 300 seconds
+        "CDN-Cache-Control": "public, s-maxage=60", //Downstream CDNs to have a TTL of 60 seconds
+        "Cache-Control": "public, s-maxage=10, stale-while-revalidate=10", //Clients to have a TTL of 10 seconds
+      },
     },
   );
 }
