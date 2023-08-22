70ba65d85190ccc02180b374f345ebc3be284ba3
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-08-22 13:08:53 +0200

    Update WatchHow.tsx

--- components/front-page/WatchHow.tsx
+++ components/front-page/WatchHow.tsx
@@ -21,17 +21,22 @@ const WatchHow = () => {
       ref={ref}
       className="flex items-center w-full bg-white h-[80px] md:h-[120px] px-3 md:px-[41px] overflow-hidden relative rounded-md"
     >
-      <div className="font-medium text-sm sm:text-lg md:text-[32px]  z-10">
+      <div className="font-medium text-sm sm:text-lg md:text-[32px] flex-1  z-10">
         {text}
       </div>
-      <a
-        className={`flex cursor-pointer center gap-2 ml-auto bg-[#DC056C] text-white rounded-md px-[20px] py-[10px] z-10 ${
-          animationState === "finished" && "animate-pop-in"
-        }`}
-      >
-        <span className="text-sm md:text-[px] font-semibold">Watch how</span>
-        <Video size={24} />
-      </a>
+      <div className="relative">
+        <a
+          className={`flex relative cursor-wait center gap-2 ml-auto opacity-70 bg-[#DC056C] text-white rounded-md px-[20px] py-[10px] z-10 ${
+            animationState === "finished" && "animate-pop-in"
+          }`}
+        >
+          <span className="text-sm md:text-[px] font-semibold">Watch how</span>
+          <Video size={24} />
+        </a>
+        <div className="absolute text-xs top-0 right-0 z-10 translate-x-[10%] -translate-y-[50%] text-white bg-orange-400 rounded-full py-1 px-2">
+          Coming soon!
+        </div>
+      </div>
       <div
         className="absolute left-[25%] top-10 w-[30%] pb-[30%] h-[0] rounded-full blur-3xl"
         style={{ backgroundColor: "rgba(250,217,255, 1)" }}
