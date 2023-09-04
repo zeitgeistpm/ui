02c83e3ed57884fafd0fcb5cf8f479d82bdf55c6
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-09-04 11:53:28 +0200

    fetch all usd price in one call

--- lib/hooks/queries/useAssetUsdPrice.ts
+++ lib/hooks/queries/useAssetUsdPrice.ts
@@ -85,24 +85,45 @@ export const useAllForeignAssetUsdPrices = (): {
   };
 };
 
+export const getBaseAssetPrices = async (): Promise<ForeignAssetPrices> => {
+  const coinGeckoIds = Object.values(FOREIGN_ASSET_METADATA).map(
+    (asset) => asset.coinGeckoId,
+  );
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/simple/price?ids=${coinGeckoIds.join(
+      "%2C",
+    )}&vs_currencies=usd`,
+  );
+  console.log(res.status);
+
+  const json = await res.json();
+
+  const assetPrices = Object.keys(
+    FOREIGN_ASSET_METADATA,
+  ).reduce<ForeignAssetPrices>((prices, assetNumber) => {
+    const assetMetadata = FOREIGN_ASSET_METADATA[Number(assetNumber)];
+    const coinGeckoId = assetMetadata.coinGeckoId;
+    const assetPrice = json[coinGeckoId].usd;
+    prices[assetNumber] = new Decimal(assetPrice);
+
+    return prices;
+  }, {});
+
+  return assetPrices;
+};
+
 //try: https://api.coingecko.com/api/v3/simple/price?ids=zeitgeist%2Cpolkadot&vs_currencies=usd
 export const getForeignAssetPriceServerSide = async (
   foreignAsset: ForeignAssetId,
 ) => {
   const coinGeckoId =
     FOREIGN_ASSET_METADATA[foreignAsset.ForeignAsset].coinGeckoId;
-  const baseUrl = process?.env?.NEXT_PUBLIC_SITE_URL;
-  console.log(
-    `${
-      baseUrl?.includes("http") ? "" : "https://"
-    }${baseUrl}/api/usd-price?asset=${coinGeckoId}`,
-  );
 
   const res = await fetch(
-    `${
-      baseUrl?.includes("http") ? "" : "https://"
-    }${baseUrl}/api/usd-price?asset=${coinGeckoId}`,
+    `https://staging.zeitgeist.pm/api/usd-price?asset=${coinGeckoId}`,
   );
+  console.log(res.status);
+
   const json = await res.json();
   console.log(json.body.price);
 
