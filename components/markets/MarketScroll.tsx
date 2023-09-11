c5f613fdbc396e492aca9aa780b30196cd31ecca
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-09-11 12:23:58 +0200

    Update MarketScroll.tsx

--- components/markets/MarketScroll.tsx
+++ components/markets/MarketScroll.tsx
@@ -40,13 +40,8 @@ const MarketScroll = ({
   const gap = 28;
 
   //calculate cards shown and width based on container width
-  const cardsShown = !hasMounted
-    ? 4
-    : windowWidth < BREAKPOINTS.md
-    ? 1
-    : windowWidth < BREAKPOINTS.lg
-    ? 2
-    : 3;
+  const cardsShown =
+    windowWidth < BREAKPOINTS.md ? 1 : windowWidth < BREAKPOINTS.lg ? 2 : 3;
 
   const cardWidth =
     windowWidth < BREAKPOINTS.md
@@ -82,6 +77,10 @@ const MarketScroll = ({
     }, 120),
     [width],
   );
+
+  if (!hasMounted) {
+    markets = markets.slice(0, cardsShown);
+  }
 
   return (
     <div
