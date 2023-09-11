389c81fe2c2c263c286d1df95e1f5d1df98cf2ad
robhyrk <rob.hyrkiel@gmail.com>
2023-09-11 14:53:22 -0400

    fix: adjust font optimization loading

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -3,7 +3,6 @@ import { useResizeDetector } from "react-resize-detector";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 
-import { ContentDimensionsProvider } from "components/context/ContentDimensionsContext";
 import TopBar from "components/menu";
 import Footer from "components/ui/Footer";
 import NotificationCenter from "components/ui/NotificationCenter";
@@ -47,24 +46,16 @@ const DefaultLayout: FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <div
-      className={`relative min-h-screen justify-evenly overflow-x-hidden ${
+      className={`${inter.variable} ${kanit.variable} ${
+        roboto_mono.variable
+      } font-sans relative min-h-screen justify-evenly overflow-x-hidden ${
         greyBackgroundPageRoutes.includes(router.pathname)
           ? "bg-light-gray"
           : ""
       }`}
     >
       <TradeItemContext.Provider value={{ data: tradeItem, set: setTradeItem }}>
-        {/* loads optimized fonts for global access */}
-        <style jsx global>
-          {`
-            :root {
-              --font-inter: ${inter.style.fontFamily};
-              --font-kanit: ${kanit.style.fontFamily};
-              --font-roboto-mono: ${roboto_mono.style.fontFamily};
-            }
-          `}
-        </style>
-        <div ref={contentRef} className="flex-grow">
+        <div ref={contentRef} className={`flex-grow`}>
           <TopBar />
           <main className="mt-24 mb-12 container-fluid" ref={mainRef}>
             <div
