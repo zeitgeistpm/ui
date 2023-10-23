20a5526fb981703e7d49673a3febadaf70d34268
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-10-23 12:45:14 +0200

    new modals

---
+++ components/liquidity/JoinPoolFormAmm2.tsx
@@ -0,0 +1,242 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { isRpcSdk, ZTG } from "@zeitgeistpm/sdk";
+import FormTransactionButton from "components/ui/FormTransactionButton";
+import Input from "components/ui/Input";
+import Decimal from "decimal.js";
+import { DEFAULT_SLIPPAGE_PERCENTAGE } from "lib/constants";
+import { useMarket } from "lib/hooks/queries/useMarket";
+import { usePool } from "lib/hooks/queries/usePool";
+import { poolTotalIssuanceRootQueryKey } from "lib/hooks/queries/useTotalIssuanceForPools";
+import { useExtrinsic } from "lib/hooks/useExtrinsic";
+import { useSdkv2 } from "lib/hooks/useSdkv2";
+import { useNotifications } from "lib/state/notifications";
+import { useEffect, useMemo, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { assetObjStringToId, PoolBalances } from "./LiquidityModal";
+
+const JoinPoolForm = ({
+  poolBalances,
+  poolId,
+  totalPoolShares,
+  baseAssetTicker,
+  onSuccess,
+}: {
+  poolBalances: PoolBalances;
+  poolId: number;
+  totalPoolShares: Decimal;
+  baseAssetTicker?: string;
+  onSuccess?: () => void;
+}) => {
+  const { register, watch, handleSubmit, setValue, getValues, formState } =
+    useForm({ reValidateMode: "onChange", mode: "all" });
+
+  const { data: pool } = usePool({ poolId });
+  const [sdk, id] = useSdkv2();
+  const notificationStore = useNotifications();
+  const [poolSharesToReceive, setPoolSharesToReceive] = useState<Decimal>();
+  const { data: market } = useMarket({ poolId });
+  const queryClient = useQueryClient();
+
+  const { send: joinPool, isLoading } = useExtrinsic(
+    () => {
+      if (isRpcSdk(sdk) && pool && poolSharesToReceive) {
+        const formValue = getValues();
+        const maxAmountsIn = pool?.weights.map((asset) => {
+          const id = assetObjStringToId(asset.assetId);
+          const assetAmount = formValue[id] ?? 0;
+          return assetAmount === ""
+            ? "0"
+            : new Decimal(assetAmount)
+                .mul(ZTG)
+                .mul((100 + DEFAULT_SLIPPAGE_PERCENTAGE) / 100)
+                .toFixed(0);
+        });
+
+        return sdk.api.tx.swaps.poolJoin(
+          poolId,
+          poolSharesToReceive.toFixed(0),
+          maxAmountsIn,
+        );
+      }
+    },
+    {
+      onSuccess: () => {
+        notificationStore.pushNotification("Joined pool", {
+          type: "Success",
+        });
+        queryClient.invalidateQueries([
+          id,
+          poolTotalIssuanceRootQueryKey,
+          poolId,
+        ]);
+        onSuccess?.();
+      },
+    },
+  );
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      if (!name) return;
+      const changedByUser = type != null;
+      const changedAsset = name;
+      const userInput = value[changedAsset];
+
+      if (name === "baseAssetPercentage" && changedByUser) {
+        const percentage = Number(value["baseAssetPercentage"]);
+        const baseBalances = poolBalances["base"];
+        const userBaseAssetBalance = baseBalances.user;
+
+        const newBaseAssetAmount = userBaseAssetBalance.mul(percentage / 100);
+        const poolToInputRatio = baseBalances.pool.div(newBaseAssetAmount);
+        for (const assetKey in poolBalances) {
+          setValue(
+            assetKey,
+            poolBalances[assetKey].pool
+              .div(poolToInputRatio)
+              .div(ZTG)
+              .toFixed(3, Decimal.ROUND_DOWN),
+            { shouldValidate: true },
+          );
+        }
+        setPoolSharesToReceive(totalPoolShares.div(poolToInputRatio));
+      } else if (
+        changedAsset != null &&
+        userInput != null &&
+        userInput !== "" &&
+        changedByUser &&
+        poolBalances
+      ) {
+        const changedAssetBalances = poolBalances[changedAsset];
+        const poolToInputRatio = changedAssetBalances.pool
+          .div(ZTG)
+          .div(userInput);
+
+        // recalculate asset amounts to keep ratio with user input
+        for (const assetKey in poolBalances) {
+          if (assetKey !== changedAsset) {
+            setValue(
+              assetKey,
+              poolBalances[assetKey].pool
+                .div(poolToInputRatio)
+                .div(ZTG)
+                .toFixed(3),
+              { shouldValidate: true },
+            );
+          }
+        }
+
+        setPoolSharesToReceive(totalPoolShares.div(poolToInputRatio));
+
+        const userBaseAssetBalance = poolBalances["base"].user;
+        const baseInputAmount = getValues("base");
+
+        setValue(
+          "baseAssetPercentage",
+          new Decimal(baseInputAmount)
+            .div(userBaseAssetBalance.div(ZTG))
+            .mul(100)
+            .toString(),
+        );
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [watch, poolBalances]);
+
+  const onSubmit: SubmitHandler<any> = () => {
+    joinPool();
+  };
+
+  const prctSharesToReceive = useMemo(() => {
+    if (!poolSharesToReceive) return new Decimal(0);
+    return poolSharesToReceive
+      .div(totalPoolShares.plus(poolSharesToReceive))
+      .mul(100);
+  }, [totalPoolShares, poolSharesToReceive]);
+
+  return (
+    <form
+      className="flex flex-col gap-y-4 md:gap-y-6"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-y-6 max-h-[250px] md:max-h-[400px] overflow-y-auto py-5">
+        {pool?.weights.map((asset, index) => {
+          const id = assetObjStringToId(asset.assetId);
+          const assetName =
+            market?.categories?.[index]?.name ?? baseAssetTicker;
+          const userAssetBalance =
+            poolBalances?.[id]?.user.div(ZTG).toNumber() ?? 0;
+
+          return (
+            <div
+              key={index}
+              className="w-full h-[56px] relative font-medium text-ztg-18-150 "
+            >
+              <div className="absolute h-full left-[15px] top-[14px] truncate w-[40%] capitalize">
+                {assetName}
+              </div>
+              <Input
+                className={`bg-anti-flash-white text-right rounded-[5px] h-[56px] px-[15px] w-full outline-none
+                            ${
+                              formState.errors[id.toString()]?.message
+                                ? "border-2 border-vermilion text-vermilion"
+                                : ""
+                            }
+              `}
+                key={index}
+                type="number"
+                step="any"
+                {...register(id.toString(), {
+                  value: 0,
+                  required: {
+                    value: true,
+                    message: "Value is required",
+                  },
+                  validate: (value) => {
+                    if (value > userAssetBalance) {
+                      return `Insufficient balance. Current balance: ${userAssetBalance.toFixed(
+                        3,
+                      )}`;
+                    } else if (value <= 0) {
+                      return "Value cannot be zero or less";
+                    }
+                  },
+                })}
+              />
+              <div className="text-vermilion text-ztg-12-120 mt-[4px]">
+                <>{formState.errors[id.toString()]?.message}</>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <input
+        className="my-[20px] px-0"
+        type="range"
+        {...register("baseAssetPercentage", { min: 0, value: "0" })}
+      />
+      {market?.status !== "Active" && (
+        <div className="bg-provincial-pink p-4 rounded-md text-sm">
+          Market is closed. Cannot provide liquidity for closed market
+        </div>
+      )}
+      <div className="flex mb-2 text-sm center gap-2">
+        <label className="block font-bold flex-1">
+          Expected Pool Ownership
+        </label>
+        {prctSharesToReceive.toFixed(1)} %
+      </div>
+
+      <FormTransactionButton
+        disabled={
+          formState.isValid === false ||
+          isLoading ||
+          market?.status !== "Active"
+        }
+      >
+        Join Pool
+      </FormTransactionButton>
+    </form>
+  );
+};
+
+export default JoinPoolForm;
---
+++ components/liquidity/LiquidityModalAmm2.tsx
@@ -0,0 +1,177 @@
+import { Dialog, Tab } from "@headlessui/react";
+import {
+  IOMarketOutcomeAssetId,
+  getIndexOf,
+  parseAssetId,
+} from "@zeitgeistpm/sdk";
+import Modal from "components/ui/Modal";
+import Decimal from "decimal.js";
+import { useAccountAssetBalances } from "lib/hooks/queries/useAccountAssetBalances";
+import { useAccountPoolAssetBalances } from "lib/hooks/queries/useAccountPoolAssetBalances";
+import { useAssetMetadata } from "lib/hooks/queries/useAssetMetadata";
+import { useBalance } from "lib/hooks/queries/useBalance";
+import { usePool } from "lib/hooks/queries/usePool";
+import { usePoolBaseBalance } from "lib/hooks/queries/usePoolBaseBalance";
+import { useTotalIssuance } from "lib/hooks/queries/useTotalIssuance";
+import { useWallet } from "lib/state/wallet";
+import { useMemo } from "react";
+import ExitPoolFormAmm2 from "./ExitPoolFormAmm2";
+import JoinPoolFormAmm2 from "./JoinPoolFormAmm2";
+
+export type PoolBalances = {
+  [key: string]: {
+    pool: Decimal; // pool total balance
+    user: Decimal; // user balance outside pool
+  };
+};
+
+export const assetObjStringToId = (assetId: string) => {
+  const asset = parseAssetId(assetId).unwrap();
+  return IOMarketOutcomeAssetId.is(asset) ? getIndexOf(asset) : "base";
+};
+
+const LiquidityModalAmm2 = ({
+  open,
+  onClose,
+  poolId,
+}: {
+  open: boolean;
+  onClose: () => void;
+  poolId: number;
+}) => {
+  const wallet = useWallet();
+  const connectedAddress = wallet.realAddress;
+  const { data: pool } = usePool({ poolId });
+
+  // pool balances
+  const { data: poolAssetBalances } = useAccountPoolAssetBalances(
+    pool?.account.accountId,
+    pool,
+  );
+
+  const { data: poolBaseBalance } = usePoolBaseBalance(poolId);
+
+  const userPoolTokensQuery = useAccountAssetBalances([
+    { account: connectedAddress, assetId: { PoolShare: poolId } },
+  ]);
+
+  const { data: totalPoolIssuance } = useTotalIssuance({ PoolShare: poolId });
+
+  const userPoolTokens =
+    connectedAddress &&
+    userPoolTokensQuery
+      ?.get(connectedAddress, {
+        PoolShare: poolId,
+      })
+      ?.data?.balance?.free.toString();
+
+  const baseAsset = pool && parseAssetId(pool.baseAsset).unrightOr(undefined);
+
+  const { data: metadata } = useAssetMetadata(baseAsset);
+
+  //user balances outside of pool
+  const { data: userBaseBalance } = useBalance(connectedAddress, baseAsset);
+  const { data: userAssetBalances } = useAccountPoolAssetBalances(
+    connectedAddress,
+    pool,
+  );
+
+  const allBalances: PoolBalances | undefined = useMemo(() => {
+    if (
+      pool?.weights &&
+      userBaseBalance &&
+      userAssetBalances &&
+      poolAssetBalances &&
+      userAssetBalances?.length !== 0 &&
+      poolAssetBalances?.length !== 0 &&
+      poolBaseBalance
+    ) {
+      const allBalances: PoolBalances = pool.weights.reduce(
+        (balances, weight, index) => {
+          const isBaseAsset = index === pool.weights.length - 1;
+
+          const userBalance = isBaseAsset
+            ? userBaseBalance
+            : new Decimal(
+                userAssetBalances == null
+                  ? 0
+                  : userAssetBalances[index].free.toString(),
+              );
+          const poolBalance = isBaseAsset
+            ? new Decimal(poolBaseBalance.toString())
+            : new Decimal(
+                poolAssetBalances == null
+                  ? 0
+                  : poolAssetBalances[index].free.toString(),
+              );
+
+          const id = assetObjStringToId(weight!.assetId);
+
+          balances[id] = {
+            pool: poolBalance,
+            user: userBalance,
+          };
+          return balances;
+        },
+        {} as PoolBalances,
+      );
+
+      return allBalances;
+    }
+  }, [
+    pool?.weights,
+    userAssetBalances,
+    userBaseBalance,
+    poolAssetBalances,
+    poolBaseBalance,
+  ]);
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Dialog.Panel className="w-full max-w-[462px] rounded-[10px] bg-white">
+        <Tab.Group>
+          <Tab.List className="flex h-[71px] text-center font-medium text-ztg-18-150">
+            <Tab className="ui-selected:font-bold ui-selected:bg-white bg-anti-flash-white transition-all w-1/2 rounded-tl-[10px]">
+              Join
+            </Tab>
+            <Tab className="ui-selected:font-bold ui-selected:bg-white bg-anti-flash-white transition-all w-1/2 rounded-tr-[10px]">
+              Exit
+            </Tab>
+          </Tab.List>
+
+          <Tab.Panels className="p-[30px]">
+            <Tab.Panel>
+              {allBalances && (
+                <JoinPoolFormAmm2
+                  poolId={poolId}
+                  poolBalances={allBalances}
+                  totalPoolShares={
+                    new Decimal(totalPoolIssuance?.toString() ?? 0)
+                  }
+                  baseAssetTicker={metadata?.symbol}
+                  onSuccess={onClose}
+                />
+              )}
+            </Tab.Panel>
+            <Tab.Panel>
+              {allBalances && (
+                <ExitPoolFormAmm2
+                  poolId={poolId}
+                  poolBalances={allBalances}
+                  totalPoolShares={
+                    new Decimal(totalPoolIssuance?.toString() ?? 0)
+                  }
+                  userPoolShares={new Decimal(userPoolTokens?.toString() ?? 0)}
+                  baseAssetTicker={metadata?.symbol}
+                  onSuccess={onClose}
+                />
+              )}
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
+      </Dialog.Panel>
+    </Modal>
+  );
+};
+
+export default LiquidityModalAmm2;
