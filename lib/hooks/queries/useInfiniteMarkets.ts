ec671c842f67da7222d5a87b20db56026a827424
robhyrk <rob.hyrkiel@gmail.com>
2023-11-07 15:48:20 -0500

    feat: filter markets on wsx by foreign asset 3

--- lib/hooks/queries/useInfiniteMarkets.ts
+++ lib/hooks/queries/useInfiniteMarkets.ts
@@ -13,6 +13,7 @@ import { MarketOutcomes } from "lib/types/markets";
 import { MarketStatus } from "@zeitgeistpm/indexer";
 import { hiddenMarketIds } from "lib/constants/markets";
 import { marketMetaFilter } from "./constants";
+import { isWSX } from "../../constants";
 
 export const rootKey = "markets-filtered";
 
@@ -67,7 +68,11 @@ export const useInfiniteMarkets = (
         status_in: statuses.length === 0 ? undefined : statuses,
         tags_containsAny: tags?.length === 0 ? undefined : tags,
         pool_isNull: withLiquidityOnly ? false : undefined,
-        baseAsset_in: currencies?.length !== 0 ? currencies : undefined,
+        baseAsset_in: isWSX
+          ? ['{"foreignAsset":3}']
+          : currencies?.length !== 0
+          ? currencies
+          : undefined,
         ...(withLiquidityOnly
           ? {
               pool: {
