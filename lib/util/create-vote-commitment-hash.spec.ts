08451af8b645620d2680314e35f8c9353a0a6700
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-11-07 12:59:56 +0100

    tests for creating commitment hash

--- lib/util/create-vote-commitment-hash.spec.ts
+++ lib/util/create-vote-commitment-hash.spec.ts
@@ -20,8 +20,17 @@ describe("createCourtCommitmentHash", () => {
       salt,
     );
 
-    expect(u8aToHex(yesHash)).toEqual(
-      "0x77d99bba0142ab7ad7e148a9cdb246ca362c03eef834481678766f639d383061",
+    const noHash = createCourtCommitmentHash(
+      sdk,
+      "dDyXkkoewJvnksMEirA7k6K76STzJz78bxYtG1Y1V2LCHJyMA",
+      {
+        CategoricalOutcome: [754 as MarketId, 1],
+      },
+      salt,
+    );
+
+    expect(u8aToHex(noHash)).toEqual(
+      "0x407fe12e135068fa36728063a30a1ce8e19c2b18ad3b471eeea2f20686e01426",
     );
   });
 });
