11d197b184230093dd330e8b666605da913801d0
robhyrk <rob.hyrkiel@gmail.com>
2023-11-16 18:15:35 -0500

    fix: filter predictions in portfolio based on base asset

--- lib/hooks/queries/constants.ts
+++ lib/hooks/queries/constants.ts
@@ -1,8 +1,10 @@
 import { MarketWhereInput } from "@zeitgeistpm/indexer";
+import { isWSX } from "lib/constants";
 
 export const marketMetaFilter: MarketWhereInput = {
   question_isNull: false,
   question_not_eq: "",
   categories_isNull: false,
   hasValidMetaCategories_eq: true,
+  baseAsset_eq: isWSX ? '{"foreignAsset":3}' : undefined,
 };
