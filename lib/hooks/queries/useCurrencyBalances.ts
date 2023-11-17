75ea73999ba9c0be60da0a8ca98c46ee4d717ae7
robhyrk <rob.hyrkiel@gmail.com>
2023-11-17 16:37:12 -0500

    merge with staging

--- lib/hooks/queries/useCurrencyBalances.ts
+++ lib/hooks/queries/useCurrencyBalances.ts
@@ -44,6 +44,7 @@ export const useCurrencyBalances = (address: string) => {
     async () => {
       if (enabled) {
         const { data } = await sdk.api.query.system.account(address);
+        //possible to add query to specify the base assset?
         const nativeBalance = calculateFreeBalance(
           data.free.toString(),
           data.miscFrozen.toString(),
