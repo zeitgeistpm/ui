6b2e0dba2c57fa4305bf5db92746b1a8d827458d
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2023-11-20 12:17:22 +0100

    add polkassembly branding

--- components/ui/ReferendumSummary.tsx
+++ components/ui/ReferendumSummary.tsx
@@ -13,33 +13,40 @@ const ReferendumSummary = ({
     <>
       {referendum && (
         <div className="w-full flex flex-col shadow-lg rounded-lg px-10 py-6 gap-4 font-medium">
-          <div className="flex">
-            <div className="text-xl">Referendum Summary</div>
+          <div className="flex items-center">
+            <div className="text-lg">Referendum</div>
             <a
               href={`https://polkadot.polkassembly.io/referenda/${referendumIndex}`}
-              className="ml-auto text-sm flex gap-2 items-center bg-ztg-blue text-white rounded-md px-2"
+              className="ml-auto text-sm flex gap-2 items-center justify-center text-black rounded-md px-2 shadow-xl py-2 "
               target="_blank"
               rel="noreferrer"
             >
-              <span>View</span>
-              <ExternalLink size={14} />
+              <img
+                src="/icons/polkassembly.svg"
+                alt="Account balance"
+                width={"80px"}
+              />
+              <ExternalLink size={12} className="absolute right-[44px]" />
             </a>
           </div>
-          <div className="h-[200px]">
-            <CircularProgressbar
-              value={barValue}
-              circleRatio={0.5}
-              strokeWidth={2}
-              styles={buildStyles({
-                rotation: 0.75,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 0.5,
-                pathColor: `#0070EB`,
-                trailColor: "#FC9965",
-              })}
-            />
+          <div className="flex items-center justify-center">
+            <div className="h-[150px] w-[300px] mt-3 ">
+              <CircularProgressbar
+                value={barValue}
+                circleRatio={0.5}
+                strokeWidth={2}
+                styles={buildStyles({
+                  rotation: 0.75,
+                  strokeLinecap: "round",
+                  textSize: "16px",
+                  pathTransitionDuration: 0.5,
+                  pathColor: `#0070EB`,
+                  trailColor: "#FC9965",
+                })}
+              />
+            </div>
           </div>
+
           <div className="flex">
             <div className="flex flex-col">
               <div className="text-[#0070EB] font-mono text-xl">
