66ce5c843dc197e1d6078eb40cd00d2480cb21a7
robhyrk <rob.hyrkiel@gmail.com>
2023-11-24 15:24:54 -0500

    fix: ensure fees are paid in WSX

--- lib/util/tx.ts
+++ lib/util/tx.ts
@@ -7,6 +7,7 @@ import type { ApiPromise } from "@polkadot/api";
 
 import { UseNotifications } from "lib/state/notifications";
 import { unsubOrWarns } from "./unsub-or-warns";
+import { isWSX } from "lib/constants";
 
 type GenericCallback = (...args: any[]) => void;
 
@@ -150,12 +151,25 @@ export const signAndSend = async (
             cb ? cb(result, unsub) : _callback(result, resolve, reject, unsub);
           },
         );
+      } else if (isWSX) {
+        const unsub = await tx.signAndSend(
+          signer,
+          {
+            ...(foreignAssetNumber != null
+              ? { assetId: foreignAssetNumber }
+              : {}),
+          },
+          (result) => {
+            cb ? cb(result, unsub) : _callback(result, resolve, reject, unsub);
+          },
+        );
       } else {
         const unsub = await tx.signAndSend(signer, (result) => {
           cb ? cb(result, unsub) : _callback(result, resolve, reject, unsub);
         });
       }
     } catch (error) {
+      console.log(error);
       reject(error);
     }
   });
