62d8ae7b3244cb489f7a05f025fe30e45aea9807
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-12-01 12:03:49 +0100

    cache tests

--- next.config.js
+++ next.config.js
@@ -14,4 +14,22 @@ module.exports = withPlaiceholder({
     ],
   },
   staticPageGenerationTimeout: 300, //5 mins
+  async headers() {
+    return [
+      {
+        // This doesn't work for 'Cache-Control' key (works for others though):
+        source: "/_next/image(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            // Instead of this value:
+            value:
+              "public, max-age=180, s-maxage=180, stale-while-revalidate=180",
+            // Cache-Control response header is `public, max-age=60` in production
+            // and `public, max-age=0, must-revalidate` in development
+          },
+        ],
+      },
+    ];
+  },
 });
--- pages/api/market/image/[marketId].tsx
+++ pages/api/market/image/[marketId].tsx
@@ -42,7 +42,7 @@ export default async function MarketImage(
 
   response.setHeader(
     "Cache-Control",
-    "public, s-maxage=600, stale-while-revalidate=259200",
+    "s-maxage=600, stale-while-revalidate=259200",
   );
 
   if (cmsData?.imageUrl) {
