b75c6be2b550298f46a945bd9cf68dc231501f2e
robhyrk <rob.hyrkiel@gmail.com>
2023-12-05 18:28:58 -0500

    chore: add wsx foreign asset id as variable

--- lib/hooks/queries/constants.ts
+++ lib/hooks/queries/constants.ts
@@ -1,11 +1,11 @@
 import { MarketWhereInput } from "@zeitgeistpm/indexer";
-import { isWSX } from "lib/constants";
+import { isWSX, wsxID } from "lib/constants";
 
 export const marketMetaFilter: MarketWhereInput = {
   question_isNull: false,
   question_not_eq: "",
   categories_isNull: false,
   hasValidMetaCategories_eq: true,
-  baseAsset_eq: isWSX ? '{"foreignAsset":3}' : undefined,
-  baseAsset_not_eq: !isWSX ? '{"foreignAsset":3}' : undefined,
+  baseAsset_eq: isWSX ? `{"foreignAsset":${wsxID}}` : undefined,
+  baseAsset_not_eq: !isWSX ? `{"foreignAsset":${wsxID}}` : undefined,
 };
