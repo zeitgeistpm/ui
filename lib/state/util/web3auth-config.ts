e9d79f03166fbc983e9611d7f19b4bb3e79a554c
robhyrk <rob.hyrkiel@gmail.com>
2023-12-10 18:16:48 -0500

    fix: env vars

--- lib/state/util/web3auth-config.ts
+++ lib/state/util/web3auth-config.ts
@@ -3,7 +3,7 @@ import { CHAIN_NAMESPACES } from "@web3auth/base";
 import { Web3Auth } from "@web3auth/modal";
 import { atom } from "jotai";
 import { isWSX } from "lib/constants";
-console.log(process.env.NODE_ENV);
+
 export const clientId = isWSX
   ? process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID_WSX
   : process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID_ZTG;
