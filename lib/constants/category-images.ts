aed1ef11f9f8ec88b1e2608121cbce90f5ac143b
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-12-19 12:17:38 +0100

    implement new images for categories

--- lib/constants/category-images.ts
+++ lib/constants/category-images.ts
@@ -1,3 +1,4 @@
+import { range } from "lodash-es";
 import { union } from "lib/types/union";
 import { prodTags } from "./markets";
 
@@ -6,14 +7,16 @@ type T = typeof prodTags | "untagged";
 const tags = [...prodTags, "untagged"] as const;
 
 export const CATEGORY_IMAGES = union<(typeof tags)[any]>().exhaustAsRecord({
-  Sports: ["/category/sports.png"],
-  Politics: ["/category/politics.png"],
-  Technology: ["/category/technology.png"],
-  Crypto: ["/category/crypto.png"],
-  Science: ["/category/science.png"],
-  "E-Sports": ["/category/e-sports.png"],
-  News: ["/category/news.png"],
-  Dotsama: ["/category/dotsama.png"],
-  Zeitgeist: ["/category/zeitgeist.png"],
-  untagged: ["/category/zeitgeist.png"],
+  Sports: range(1, 6).map((i) => `/categories/sports/${i}.png`),
+  Politics: range(1, 5).map((i) => `/categories/politics/${i}.png`),
+  Technology: range(1, 6).map((i) => `/categories/tech/${i}.png`),
+  Crypto: range(1, 5).map((i) => `/categories/crypto/${i}.png`),
+  Science: range(1, 5).map((i) => `/categories/science/${i}.png`),
+  "E-Sports": range(1, 6).map((i) => `/categories/esports/${i}.png`),
+  News: range(1, 6).map((i) => `/categories/news/${i}.png`),
+  Dotsama: range(1, 2).map((i) => `/categories/dotsama/${i}.png`),
+  Zeitgeist: range(1, 2).map((i) => `/categories/zeitgeist/${i}.png`),
+  untagged: range(1, 2).map((i) => `/categories/zeitgeist/${i}.png`),
 });
+
+console.log(CATEGORY_IMAGES);
