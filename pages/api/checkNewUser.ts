cb1936e728b801819dad8398dd05d841ebfe1503
robhyrk <rob.hyrkiel@gmail.com>
2023-12-18 23:54:37 -0500

    fix api settings

--- pages/api/checkNewUser.ts
+++ pages/api/checkNewUser.ts
@@ -5,14 +5,14 @@ import { nttID } from "lib/constants";
 
 export default async function checkNewUser(req, res) {
   if (
-    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
-    !process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY
+    !process.env.NEXT_PUBLIC_SUPABASE_URL_NTT ||
+    !process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY_NTT
   ) {
     return;
   }
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL_NTT,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY_NTT,
     { auth: { persistSession: false } },
   );
 
@@ -55,7 +55,7 @@ export default async function checkNewUser(req, res) {
 }
 
 async function fundUser(wallet) {
-  if (!process.env.NEXT_PUBLIC_SEED) {
+  if (!process.env.NEXT_PUBLIC_SEED_NTT) {
     return { error: "Error connecting" };
   }
   const provider = new WsProvider("wss://bsr.zeitgeist.pm");
@@ -63,7 +63,7 @@ async function fundUser(wallet) {
 
   try {
     const keyring = new Keyring({ type: "sr25519" });
-    const masterAccount = keyring.addFromUri(process.env.NEXT_PUBLIC_SEED);
+    const masterAccount = keyring.addFromUri(process.env.NEXT_PUBLIC_SEED_NTT);
 
     // const amount = 1_000_000_000_000_0; // 1000 tokens
     const amount = 1_000_000_000_000; // 100 tokens
