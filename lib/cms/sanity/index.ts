e362a56f26dbbbc1ec756426048d09ca8aa4da3c
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2024-01-09 12:47:51 +0100

    use sanity dataset based on env

--- lib/cms/sanity/index.ts
+++ lib/cms/sanity/index.ts
@@ -1,12 +1,13 @@
 import { createClient, type ClientConfig } from "@sanity/client";
+import { environment } from "lib/constants";
 
-const PROJECT_ID = "4wbnjof1"; // TODO: move to env
-const VERSION = "2022-03-07"; // TODO: move to env
-const DATA_SET = "bsr"; // TODO: move to env
+const PROJECT_ID = "4wbnjof1";
+const VERSION = "2022-03-07";
+const DATASET = environment === "production" ? "mainnet" : "bsr";
 
 const config: ClientConfig = {
   projectId: PROJECT_ID,
-  dataset: DATA_SET,
+  dataset: DATASET,
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: VERSION, // use current date (YYYY-MM-DD) to target the latest API version
 };
