888faf0eedae65442a757411ee1f63a210ba5bc7
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2024-01-11 12:25:08 +0100

    Update news.ts

--- lib/cms/news.ts
+++ lib/cms/news.ts
@@ -10,17 +10,6 @@ export type CmsNews = {
     | { isMarket: false | undefined; url: string };
 };
 
-const DEFAULT_NEWS: CmsNews[] = [
-  {
-    title: "Defaut Banner Title",
-    subtitle: "Default Subtitle",
-    image:
-      "https://images.unsplash.com/photo-1680523127490-978b85b8bf71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
-
-    link: { isMarket: false, url: "#" },
-  },
-];
-
 const fields = groq`{
   title,
   subtitle,
@@ -29,11 +18,6 @@ const fields = groq`{
 }`;
 
 export const getCmsNews = async (): Promise<CmsNews[]> => {
-  // Short circuit to use default if NOTION_API_KEY doesn't exist.
-  if (!process.env.NOTION_API_KEY) {
-    return DEFAULT_NEWS;
-  }
-
   const data = await sanity.fetch<CmsNews[]>(
     groq`*[_type == "news"] | order(_createdAt desc) ${fields}`,
   );
