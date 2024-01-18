28d0fb5395876919446ab02947c56a54545faf48
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2024-01-18 15:23:39 +0200

    fix query

--- lib/hooks/queries/useIdentities.ts
+++ lib/hooks/queries/useIdentities.ts
@@ -12,61 +12,64 @@ export const useIdentities = (addresses?: string[]) => {
     [id, identitiesRootKey, addresses],
     async () => {
       if (addresses && isRpcSdk(sdk)) {
+        console.log(addresses);
+
         const userIdentities: UserIdentity[] = [];
-        sdk.api.queryMulti(
+        const res = await sdk.api.queryMulti(
           addresses.map((address) => [
             sdk.api.query.identity.identityOf as any,
             address,
           ]),
-          (res) => {
-            res.forEach((identity: any) => {
-              const indentityInfo =
-                identity.isNone === false
-                  ? (identity.value as any).get("info")
-                  : undefined;
-              if (indentityInfo) {
-                const textDecoder = new TextDecoder();
+        );
 
-                let discordHandle: string | undefined;
-                indentityInfo.get("additional").forEach((element) => {
-                  if (
-                    element[0].value?.isEmpty === false &&
-                    textDecoder.decode(element[0].value)
-                  ) {
-                    discordHandle = textDecoder.decode(element[1].value);
-                  }
-                });
+        console.log(res);
 
-                const judgements = identity.value.get("judgements")[0];
+        res.forEach((identity: any) => {
+          const indentityInfo =
+            identity.isNone === false
+              ? (identity.value as any).get("info")
+              : undefined;
+          if (indentityInfo) {
+            const textDecoder = new TextDecoder();
 
-                const judgementType: Judgement = judgements
-                  ? judgements[1].type
-                  : "Unknown";
-
-                const userIdentity: UserIdentity = {
-                  displayName:
-                    indentityInfo.get("display").isNone === false
-                      ? textDecoder.decode(indentityInfo.get("display").value)
-                      : "",
-                  twitter:
-                    indentityInfo.get("twitter").isNone === false
-                      ? textDecoder.decode(indentityInfo.get("twitter").value)
-                      : "",
-                  discord: discordHandle,
-                  judgement: judgementType,
-                };
-
-                userIdentities.push(userIdentity);
-              } else {
-                const userIdentity: UserIdentity = {
-                  displayName: "",
-                };
-
-                userIdentities.push(userIdentity);
+            let discordHandle: string | undefined;
+            indentityInfo.get("additional").forEach((element) => {
+              if (
+                element[0].value?.isEmpty === false &&
+                textDecoder.decode(element[0].value)
+              ) {
+                discordHandle = textDecoder.decode(element[1].value);
               }
             });
-          },
-        );
+
+            const judgements = identity.value.get("judgements")[0];
+
+            const judgementType: Judgement = judgements
+              ? judgements[1].type
+              : "Unknown";
+
+            const userIdentity: UserIdentity = {
+              displayName:
+                indentityInfo.get("display").isNone === false
+                  ? textDecoder.decode(indentityInfo.get("display").value)
+                  : "",
+              twitter:
+                indentityInfo.get("twitter").isNone === false
+                  ? textDecoder.decode(indentityInfo.get("twitter").value)
+                  : "",
+              discord: discordHandle,
+              judgement: judgementType,
+            };
+
+            userIdentities.push(userIdentity);
+          } else {
+            const userIdentity: UserIdentity = {
+              displayName: "",
+            };
+
+            userIdentities.push(userIdentity);
+          }
+        });
         return userIdentities;
       }
     },
