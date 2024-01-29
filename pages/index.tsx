30a7812d058f219f883f3962ade063b524520bd1
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2024-01-29 12:40:18 +0200

    Revalidate less on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -116,7 +116,7 @@ export async function getStaticProps() {
     },
     revalidate:
       environment === "production"
-        ? 1 * 60 //1min
+        ? 5 * 60 //5min
         : 60 * 60,
   };
 }
