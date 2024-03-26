3058dd9736b3b2a3adcae8665a277cdbeffe8e15
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2024-03-26 13:02:48 +0100

    improve robustness of extracting channel name and determining live status

--- lib/twitch/index.ts
+++ lib/twitch/index.ts
@@ -1,11 +1,18 @@
-export const isLive = async (channelName: string) => {
-  const res = await fetch(`https://decapi.me/twitch/uptime/${channelName}`);
+import { isAbsoluteUrl } from "next/dist/shared/lib/utils";
+
+export const isLive = async (channelNameOrUrl: string) => {
+  const res = await fetch(
+    `https://decapi.me/twitch/uptime/${extractChannelName(channelNameOrUrl)}`,
+  );
   const data = await res.text();
-  console.log("data", data);
-  return !data.match("is offline");
+  return res.ok && !data.match(/is offline|error/i);
 };
 
 export const extractChannelName = (url?: string) => {
   if (!url) return null;
-  return new URL(url ?? "").pathname.replace("/", "");
+  if (isAbsoluteUrl(url)) {
+    return new URL(url ?? "").pathname.replace("/", "");
+  } else {
+    return url;
+  }
 };
