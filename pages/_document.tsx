fe6887585f0041711ad27c9a8be58da92601545f
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2024-04-10 16:43:02 +0100

    update tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,10 +20,15 @@ export default function Document() {
           </Script>
         )}
         {useAnalytics && (
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-J3881Y08L6"
-            strategy="afterInteractive"
-          />
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5KVRHBT');
+            `}
+          </Script>
         )}
         <meta
           name="description"
@@ -62,6 +67,14 @@ export default function Document() {
         />
       </Head>
       <body className="overflow-x-hidden">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KVRHBT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
