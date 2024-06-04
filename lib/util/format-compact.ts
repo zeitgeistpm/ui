f0f67c391e8b6908e87d8a4b378656d495f812bf
robhyrk <rob.hyrkiel@gmail.com>
2024-06-04 15:29:43 -0400

    feat: localize number formatting based on user browser settings

--- lib/util/format-compact.ts
+++ lib/util/format-compact.ts
@@ -2,10 +2,11 @@ export const formatNumberCompact = (
   num: number | bigint,
   maximumSignificantDigits = 3,
 ) => {
+  const userLocale = navigator.language || "en-US";
   // Ensure displaying absolute zeros are unsigned(-), because javascript sucks sometimes.
   if (num === 0 || num === 0n) num = 0;
 
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat(userLocale, {
     maximumSignificantDigits: maximumSignificantDigits,
     notation: "compact",
   }).format(num);
