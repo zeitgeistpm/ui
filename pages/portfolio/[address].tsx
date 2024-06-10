da8bfefce09531aebcafbaa38331bda7408cbb6e
Tom Robiquet <tom-robiquet@hotmail.co.uk>
2024-06-10 15:27:54 +0100

    comment position filtering

--- pages/portfolio/[address].tsx
+++ pages/portfolio/[address].tsx
@@ -133,9 +133,9 @@ const Portfolio: NextPageWithLayout = () => {
                   (marketPositions) => {
                     const market = marketPositions[0].market;
 
-                    marketPositions = marketPositions.filter((position) =>
-                      position.userBalance.gt(0),
-                    );
+                    // marketPositions = marketPositions.filter((position) =>
+                    //   position.userBalance.gt(0),
+                    // );
 
                     if (
                       market.status === "Resolved" &&
@@ -156,9 +156,12 @@ const Portfolio: NextPageWithLayout = () => {
                         className="mb-8"
                         market={market}
                         usdZtgPrice={ztgPrice}
-                        positions={marketPositions.filter((position) =>
-                          position.userBalance.gt(0),
-                        )}
+                        positions={
+                          marketPositions
+                          // .filter((position) =>
+                          // position.userBalance.gt(0),
+                          // )
+                        }
                       />
                     );
                   },
